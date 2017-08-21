6fef18f20b18e688653959f227a9abf8e31bab2f
Brandon Yeo <brandonyeoxg@gmail.com>
2017-08-21 23:16:58 +0800

    Implement Parser as a class

--- source/Parser.cpp
+++ source/Parser.cpp
@@ -1,16 +1,25 @@
 #pragma once
 
-#include<stdio.h>
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
+#include "Parser.h"
 #include "PKB.h"
 #include "TNode.h"
 
-int Parse () {
-
+int Parser::parse () {
+  // Open file
+  std::string stmtLine;
+  ifstream readStream(m_filename);
+  if (readStream.is_open()) {
+    while (getline(readStream, stmtLine)) {
+      std::cout << stmtLine << "\n";
+    }
+  }
 	return 0;
 }
