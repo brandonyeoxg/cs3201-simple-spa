b68cbfea584c4f3a094960176f11740e1ffa584f
brandonyeoxg <brandonyeoxg@gmail.com>
2017-08-24 21:22:25 +0800

    Extend functionality of StringUtil to make string into lower case.

--- source/StringUtil.h
+++ source/StringUtil.h
@@ -7,8 +7,9 @@ class StringUtil
 public:
   StringUtil();
   ~StringUtil();
-  static std::vector<std::string> splitString(std::string t_line, char t_delim);
+  static std::vector<std::string> splitString(std::string t_line, char t_delim = ' ');
   static std::string trimString(const std::string &str, const std::string &whitespace = " \t");
-  static std::string reduceString(const std::string& str, const std::string& fill = " ", const std::string& whitespace = " \t");
+  static std::string reduceString(const std::string &str, const std::string& fill = " ", const std::string& whitespace = " \t");
+  static void toLower(std::string &str);
 };
 
