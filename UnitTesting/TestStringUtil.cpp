6efd8b78a266469fb7bc975beee663ee909581c5
brandonyeoxg <brandonyeoxg@gmail.com>
2017-08-24 19:32:39 +0800

    Add tests for StringUtil

--- UnitTesting/TestStringUtil.cpp
+++ UnitTesting/TestStringUtil.cpp
@@ -29,5 +29,18 @@ namespace UnitTesting {
       std::string expected = "Hello world";
       Assert::AreEqual(actual, expected);
     }
+
+    TEST_METHOD(splitStringWithReduceString) {
+      std::string normalInput = "   Hello  World   ";
+      std::vector<std::string> actual;
+      normalInput = StringUtil::reduceString(normalInput);
+      actual = StringUtil::splitString(normalInput, ' ');
+
+      std::vector<std::string> expected = { "Hello", "World" };
+
+      for (unsigned int i = 0; i < actual.size(); i++) {
+        Assert::AreEqual(expected.at(i), actual.at(i));
+      }
+    }
   };
 }
