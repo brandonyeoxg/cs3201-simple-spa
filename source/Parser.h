0a74c0f4ca8e7bd29f82d3c6e4977b6e27a58c48
brandonyeoxg <brandonyeoxg@gmail.com>
2017-08-24 21:23:40 +0800

    Extend Parser functionality to include brace matching and reading of procedures.

--- source/Parser.h
+++ source/Parser.h
@@ -1,16 +1,29 @@
 #pragma once
+#include <stack>
+#include "PKB.h"
 using namespace std;
-
 /*
   Represents the parser that parser a file.
 */
 class Parser {
 public:
-  Parser(std::string &t_filename) 
-    : m_filename(t_filename) {};
+  Parser(PKB *t_pkb) 
+    : m_pkb(t_pkb), 
+      m_curLineNum(0),
+      m_curProcNum(0) {};
+
   ~Parser() {};
-  int parse(); //! < returns 0 if no issue, -1 if there is a problem.
+  
+  int parse(const std::string &t_filename); //! < returns 0 if no issue, -1 if there is a problem.
 
 private:
-  std::string m_filename;
+  unsigned int m_curLineNum;
+  PKB *m_pkb;
+  PROC m_curProcNum;
+  std::stack<char> m_bracesStack;
+
+  int parseForProcedure(ifstream &t_readStream, const std::string &t_line);
+  int parseCodeInProcedure(ifstream &t_readStream);
+  bool parseForBraces(vector<string> &t_tokens);
+  std::vector<std::string> tokeniseLine(const std::string &t_line);
 };
