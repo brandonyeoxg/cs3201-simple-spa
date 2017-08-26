1a415fba6d338c08840953bdaf1dc28ff611ce6f
turtle96 <angjazlyn@hotmail.com>
2017-08-27 01:53:06 +0800

    add constant node test rewrtie test

--- UnitTesting/TestAST.cpp
+++ UnitTesting/TestAST.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 #include "TNode.h"
 #include "VariableNode.h"
+#include "ConstantNode.h"
 #include "ASTBuilder.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -14,16 +15,30 @@ public:
     // TODO: Your test code here
   }
 
-  TEST_METHOD(TestVariableNodeConstructor) {
-    Logger::WriteMessage("Running Variable Node constructor test");
+  TEST_METHOD(TestConstantNode) {
+    Logger::WriteMessage("Running Constant Node test");
+
+    int lineNum = 5;
+    int value = 100;
+    TNode *tNode = new ConstantNode(lineNum, value);
+    Assert::IsTrue(tNode->getType() == TNode::Type::Constant);
+    Assert::IsTrue(tNode->getLineNum() == lineNum);
+    Assert::IsTrue(tNode->getChildren() == nullptr);
+
+    ConstantNode *constNode = (ConstantNode *) tNode;
+    Assert::IsTrue(constNode->getValue() == value);
+  }
+
+  TEST_METHOD(TestVariableNode) {
+    Logger::WriteMessage("Running Variable Node test");
 
     int lineNum = 5;
     std::string varName = "penguin";
     TNode *tNode = new VariableNode(lineNum, varName);
-    Assert::IsTrue(tNode->getLineNum() == lineNum);
     Assert::IsTrue(tNode->getType() == TNode::Type::Variable);
+    Assert::IsTrue(tNode->getLineNum() == lineNum);
 
-    VariableNode *varNode = new VariableNode(lineNum, varName);
+    VariableNode *varNode = (VariableNode *) tNode;
     Assert::IsTrue(varNode->getVarName() == varName);
 
     varNode = new VariableNode();
