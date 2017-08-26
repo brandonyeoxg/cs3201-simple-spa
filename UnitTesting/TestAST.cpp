ee3c3820a54ffb167fb9d5c140ca7b4cf7adfacc
turtle96 <angjazlyn@hotmail.com>
2017-08-26 18:12:59 +0800

    test VariableNode constructors

--- UnitTesting/TestAST.cpp
+++ UnitTesting/TestAST.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "TNode.h"
+#include "VariableNode.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -14,6 +15,22 @@ namespace UnitTesting
     {
       TNode T;
       // TODO: Your test code here
+    }
+
+    TEST_METHOD(TestTNodeConstructor) {
+      Logger::WriteMessage("Running TNode constructor test");
+
+      int lineNum = 5;
+      std::string varName = "penguin";
+      TNode *tNode = new VariableNode(lineNum, varName);
+      Assert::IsTrue(tNode->getLineNum() == lineNum);
+      Assert::IsTrue(tNode->getType() == TNode::Type::Variable);
+
+      VariableNode *varNode = new VariableNode(lineNum, varName);
+      Assert::IsTrue(varNode->getVarName() == varName);
+
+      varNode = new VariableNode();
+      Assert::IsFalse(varNode->getLineNum() == lineNum);
     }
 
   };
