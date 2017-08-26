a78e4f22a0e583036f1a8b3890c00d0eb2e1c035
brandonyeoxg <brandonyeoxg@gmail.com>
2017-08-26 14:12:54 +0800

    Attempt to make parser more modular.

--- source/Parser.h
+++ source/Parser.h
@@ -2,15 +2,21 @@
 #include <stack>
 #include "PKB.h"
 using namespace std;
-/*
-  Represents the parser that parser a file.
-*/
+
+/**
+ * Represents a parser. Parses the SIMPLE program and builds an ast.
+ *
+ * @author Brandon
+ * @date 8/26/2017
+ *
+ */
 class Parser {
 public:
   Parser(PKB *t_pkb) 
     : m_pkb(t_pkb), 
       m_curLineNum(0),
-      m_curProcNum(0) {};
+      m_curProcNum(0),
+      m_nextToken("") {};
 
   ~Parser() {};
   
@@ -20,6 +26,7 @@ private:
   unsigned int m_curLineNum;
   PKB *m_pkb;
   PROC m_curProcNum;
+  std::string m_nextToken;
   std::stack<std::string> m_bracesStack;
 
   int parseForProcedure(ifstream &t_readStream, const std::string &t_line);
@@ -27,5 +34,7 @@ private:
   int parseLine(std::vector<std::string> t_tokens);
   bool parseForBraces(const string &t_token);
   bool parseForVariable(const string &t_token);
+
+  bool matchToken(const std::string &t_token, ifstream &t_readStream);
   std::vector<std::string> tokeniseLine(const std::string &t_line);
 };
