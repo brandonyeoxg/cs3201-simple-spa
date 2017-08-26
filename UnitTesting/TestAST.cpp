6a18e5c77d103b4fd35217755223f436c2881ffc
turtle96 <angjazlyn@hotmail.com>
2017-08-26 22:05:42 +0800

    format file

--- UnitTesting/TestAST.cpp
+++ UnitTesting/TestAST.cpp
@@ -5,33 +5,30 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace UnitTesting
-{
-  TEST_CLASS(TestAST)
-  {
-  public:
+namespace UnitTesting {
+  TEST_CLASS(TestAST) {
+public:
 
-    TEST_METHOD(TestMethod1)
-    {
-      TNode T;
-      // TODO: Your test code here
-    }
+  TEST_METHOD(TestMethod1) {
+    TNode T;
+    // TODO: Your test code here
+  }
 
-    TEST_METHOD(TestTNodeConstructor) {
-      Logger::WriteMessage("Running TNode constructor test");
+  TEST_METHOD(TestTNodeConstructor) {
+    Logger::WriteMessage("Running TNode constructor test");
 
-      int lineNum = 5;
-      std::string varName = "penguin";
-      TNode *tNode = new VariableNode(lineNum, varName);
-      Assert::IsTrue(tNode->getLineNum() == lineNum);
-      Assert::IsTrue(tNode->getType() == TNode::Type::Variable);
+    int lineNum = 5;
+    std::string varName = "penguin";
+    TNode *tNode = new VariableNode(lineNum, varName);
+    Assert::IsTrue(tNode->getLineNum() == lineNum);
+    Assert::IsTrue(tNode->getType() == TNode::Type::Variable);
 
-      VariableNode *varNode = new VariableNode(lineNum, varName);
-      Assert::IsTrue(varNode->getVarName() == varName);
+    VariableNode *varNode = new VariableNode(lineNum, varName);
+    Assert::IsTrue(varNode->getVarName() == varName);
 
-      varNode = new VariableNode();
-      Assert::IsFalse(varNode->getLineNum() == lineNum);
-    }
+    varNode = new VariableNode();
+    Assert::IsFalse(varNode->getLineNum() == lineNum);
+  }
 
   };
 }
