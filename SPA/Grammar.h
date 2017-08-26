8c95bf21e96075a816816ce3329a27af911a0042
Ryan <ghost_heartbeat@yahoo.com.sg>
2017-08-26 17:28:31 +0800

    Created file for Design Abstraction Object.
    Reformatted Grammar.cpp to separate the declaration and implementation of Grammar.
    
    Finished Grammar.h

--- SPA/Grammar.h
+++ SPA/Grammar.h
@@ -1,8 +1,18 @@
 #pragma once
+#ifndef GRAMMAR_H
+#define GRAMMAR_H
+
 class Grammar
 {
 public:
-  Grammar();
-  ~Grammar();
+  enum class GType { PROC, ST_LST, STMT, ASGN, WHILE, EXPR, VAR, CONST };
+  Grammar(int t_type, string t_name);
+  GType getType() { return m_type; }
+  string getMonth() { return m_name; }
+
+private:
+  GType m_type; /**< type of this Grammar object */
+  string m_name; /**< name of this Grammar object */
 };
 
+#endif
