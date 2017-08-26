d754a6e03ffff7beb30d5a24675b398224ba4769
brandonyeoxg <brandonyeoxg@gmail.com>
2017-08-27 00:52:03 +0800

    Add StringUtil tests.

--- UnitTesting/TestStringUtil.cpp
+++ UnitTesting/TestStringUtil.cpp
@@ -9,38 +9,50 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace UnitTesting {
   TEST_CLASS(TestStringUtil) {
   public:
-    TEST_METHOD(splitString) {
-      std::string normalInput = "Hello World";
-      std::vector<std::string> actual;
-      actual = StringUtil::splitString(normalInput, ' ');
-
-      std::vector<std::string> expected = { "Hello", "World" };
-
-      for (unsigned int i = 0; i < actual.size(); i++) {
-        Assert::AreEqual(expected.at(i), actual.at(i));
-      }
-    }
-
     TEST_METHOD(reduceString) {
-      std::string normalInput = " Hello  world  ";
-
-      std::string actual = StringUtil::reduceString(normalInput);
-
       std::string expected = "Hello world";
+
+      std::string normalInput = " Hello  world  ";
+      std::string actual = StringUtil::reduceString(normalInput);
+      Assert::AreEqual(actual, expected);
+
+      normalInput = "Hello world";
+      actual = StringUtil::reduceString(normalInput);
+      Assert::AreEqual(actual, expected);
+
+      normalInput = "Helloworld";
+      actual = StringUtil::reduceString(normalInput);
+      Assert::AreNotEqual(actual, expected);
+
+      normalInput = "";
+      expected = "";
+      actual = StringUtil::reduceString(normalInput);
       Assert::AreEqual(actual, expected);
     }
 
-    TEST_METHOD(splitStringWithReduceString) {
-      std::string normalInput = "   Hello  World   ";
-      std::vector<std::string> actual;
-      normalInput = StringUtil::reduceString(normalInput);
-      actual = StringUtil::splitString(normalInput, ' ');
+    TEST_METHOD(trimString) {
+      std::string expected = "Hello world";
 
-      std::vector<std::string> expected = { "Hello", "World" };
+      std::string normalInput = " Hello world ";
+      std::string actual = StringUtil::trimString(normalInput);
+      Assert::AreEqual(actual, expected);
 
-      for (unsigned int i = 0; i < actual.size(); i++) {
-        Assert::AreEqual(expected.at(i), actual.at(i));
-      }
+      normalInput = "Hello world ";
+      actual = StringUtil::trimString(normalInput);
+      Assert::AreEqual(actual, expected);
+
+      normalInput = " Hello world";
+      actual = StringUtil::trimString(normalInput);
+      Assert::AreEqual(actual, expected);
+
+      normalInput = " ";
+      expected = "";
+      actual = StringUtil::trimString(normalInput);
+      Assert::AreEqual(actual, expected);
+
+      normalInput = "";
+      actual = StringUtil::trimString(normalInput);
+      Assert::AreEqual(actual, expected);
     }
   };
 }
