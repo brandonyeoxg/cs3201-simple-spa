4e971d55f4a926610dbce8b67c9bcdff7a626d6e
turtle96 <angjazlyn@hotmail.com>
2017-08-27 21:21:20 +0800

    switch arrangement

--- source/VariableNode.cpp
+++ source/VariableNode.cpp
@@ -11,9 +11,9 @@ VariableNode::VariableNode() {
 VariableNode::VariableNode(int t_lineNum, std::string t_varName) {
   m_type = TNode::Type::Variable;
   m_lineNum = t_lineNum;
-  m_varName = t_varName;
   m_children = nullptr;
   m_parent = nullptr;
+  m_varName = t_varName;
 }
 
 std::string VariableNode::getVarName() {
