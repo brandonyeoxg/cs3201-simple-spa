afa8385e8ecc6a23e320b92bf85c773655da87ad
turtle96 <angjazlyn@hotmail.com>
2017-08-27 21:31:05 +0800

    add parameters to API

--- source/ASTBuilderAPI.h
+++ source/ASTBuilderAPI.h
@@ -12,9 +12,9 @@ public:
   virtual AST *createAST() = 0;
   virtual TNode *createConstant(int t_lineNum, int t_constantValue) = 0;
   virtual TNode *createVariable(int t_lineNum, std::string t_varName) = 0;
-  virtual TNode *createProcedure(std::string procName) = 0;
+  virtual TNode *createProcedure(std::string t_procName) = 0;
   virtual TNode *createStmtList() = 0;
-  virtual TNode *buildAssignment() = 0;
+  virtual TNode *buildAssignment(int t_lineNum, TNode *t_leftNode, TNode *t_rightNode) = 0;
   virtual TNode *buildWhile() = 0;
   virtual TNode *buildAddition() = 0;
   virtual TNode *buildSubtraction() = 0;
