ad1c7102bdb8af1264a233ba530b77f8f022fbe7
pengcheng229 <pengcheng@hive-up.com>
2017-08-27 21:06:28 +0800

    Create FollowTable

---
+++ SPA/FollowTable.cpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+#include "FollowTable.h"
+
+void FollowTable::insert(int s1, int s2) {
+}
+
+vector<int> FollowTable::getS1(int s2) {
+  return vector<int>();
+}
+
+vector<int> FollowTable::getS2(int s1) {
+  return vector<int>();
+}
+
+FollowTable::FollowTable() {
+}
---
+++ SPA/FollowTable.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+/**
+* Represents the follow relationship table, which is a vector of unordered_maps to
+* track which line follows which lines.
+*
+* @author pengcheng
+* @date 8/27/2017
+*
+*/
+class FollowTable {
+public:
+  void insert(int s1, int s2);
+  vector<int> getS1(int s2);
+  vector<int> getS2(int s1);
+  FollowTable();
+
+protected:
+  unordered_map<int, vector<int>> followTable;
+};
