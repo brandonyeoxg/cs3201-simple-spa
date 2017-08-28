a97280cfedf2b1805e79b0ad12e80d8c8d7ec02c
pengcheng229 <pengcheng@hive-up.com>
2017-08-28 16:58:57 +0800

    add test S1 & S2 for follow table

--- UnitTesting/TestFollowTable.cpp
+++ UnitTesting/TestFollowTable.cpp
@@ -7,16 +7,14 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace UnitTesting {
   TEST_CLASS(TestFollowTable) {
   public:
-
+    unordered_map<int, std::vector<int>> test = {
+      { 1,{ 2, 3 } },
+      { 2,{ 3, 4 } },
+      { 3,{ 4 } }
+    };
     TEST_METHOD(TestInsert) {
-      Logger::WriteMessage("Running follow table test");
+      Logger::WriteMessage("Running follow table test insert");
       FollowTable testFollowTable;
-      unordered_map<int, std::vector<int>> test = {
-        { 1, { 2, 3 } },
-        { 2, { 3, 4 } },
-        { 3, { 4 } }
-      }; 
-      
       testFollowTable.setFollowTable(test);
       testFollowTable = testFollowTable.insert(testFollowTable, 4, 5);
       unordered_map<int, std::vector<int>> testFollowTableResult;
@@ -31,6 +29,26 @@ namespace UnitTesting {
       //Assert::IsTrue(2 == 2);
       //Assert:AreEquals
       //return;
+    }
+
+    TEST_METHOD(TestGetS1) {
+      Logger::WriteMessage("Running follow table test getS1");
+      static const int arr[] = { 1, 2 };
+      std::vector<int> expected (arr, arr + sizeof(arr) / sizeof(arr[0]));
+      FollowTable testFollowTable;
+      testFollowTable.setFollowTable(test);
+      vector<int> actual = testFollowTable.getS1(3);
+      Assert::IsTrue(actual == expected);
+    }
+
+    TEST_METHOD(TestGetS2) {
+      Logger::WriteMessage("Running follow table test getS2");
+      static const int arr[] = { 3, 4 };
+      std::vector<int> expected(arr, arr + sizeof(arr) / sizeof(arr[0]));
+      FollowTable testFollowTable;
+      testFollowTable.setFollowTable(test);
+      vector<int> actual = testFollowTable.getS2(2);
+      Assert::IsTrue(actual == expected);
     }
   };
 }
