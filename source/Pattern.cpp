36458016516e1d30fd4edf73eef816a82911ba64
Verbena <verbena_94@hotmail.com>
2017-08-31 01:57:57 +0800

    Add Grammar to the Pattern Class. (Grammar as in what statements is the pattern for)

--- source/Pattern.cpp
+++ source/Pattern.cpp
@@ -13,11 +13,18 @@ Pattern::Pattern() {}
 * A constructor.
 * The constructor takes in the values of the pattern clause in the queries.
 */
-Pattern::Pattern(std::string t_left, std::string t_right, bool t_subtree) {
+Pattern::Pattern(Grammar t_grammar, std::string t_left, std::string t_right, bool t_subtree) {
+  m_grammar = t_grammar;
   m_left = t_left;
   m_right = t_right;
   m_subtree = t_subtree;
 }
+
+/**
+* A Getter that returns the grammar of the pattern clause.
+* Returns the grammar of the pattern clause.
+*/
+Grammar Pattern::getGrammar() { return m_grammar; }
 
 /**
 * A Getter that returns the left string of the pattern clause.
@@ -36,6 +43,13 @@ std::string Pattern::getRight() { return m_right; }
 * Returns a boolean of whether the pattern clause includes the subtree.
 */
 bool Pattern::getSubtree() { return m_subtree; }
+
+/**
+* A Setter that sets the grammar of the pattern clause.
+*/
+void Pattern::setGrammar(Grammar t_grammar) {
+  m_grammar = t_grammar;
+}
 
 /**
 * A Setter that sets the left string of the pattern clause.
--- source/Pattern.h
+++ source/Pattern.h
@@ -5,24 +5,30 @@
 #include <string>
 #include <vector>
 
+#include "Grammar.h"
+
 #ifndef PATTERN_H
 #define PATTERN_H
 
 class Pattern {
 public:
   Pattern();
-  Pattern(std::string t_left, std::string t_right, bool t_subtree);
+  Pattern(Grammar t_grammar, std::string t_left, std::string t_right, bool t_subtree);
+  Grammar getGrammar();
   std::string getLeft();
   std::string getRight();
   bool getSubtree();
+  void setGrammar(Grammar);
   void setLeft(std::string);
   void setRight(std::string);
   void setSubtree(bool);
 
 private:
-  std::string m_left;
-  std::string m_right;
-  bool m_subtree;
+  Grammar m_grammar; /**< grammar of the pattern clause */
+  std::string m_left; /**< left hand side of the pattern clause */
+  std::string m_right; /**< right hand side of the pattern clause */
+  bool m_subtree; /**< true if m_right can be a sub-expression of the expression on the right hand side
+                       false if the expression on the right hand side has to be the exact match as m_right*/
 };
 
 #endif PATTERN_H
