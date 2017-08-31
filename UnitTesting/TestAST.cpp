b4c87b63c85e9706decf9cf0b2ef5a0fb7666449
turtle96 <angjazlyn@hotmail.com>
2017-08-31 22:35:04 +0800

    refactor test, extract code to 2 helper functions to check assertions

--- UnitTesting/TestAST.cpp
+++ UnitTesting/TestAST.cpp
@@ -17,8 +17,8 @@ public:
     int lineNum = 5;
     int value = 100;
     TNode *tNode = new ConstantNode(lineNum, value);
-    Assert::IsTrue(tNode->getType() == TNode::Type::Constant);
-    Assert::IsTrue(tNode->getLineNum() == lineNum);
+    isEqualType(tNode, TNode::Type::Constant);
+    isEqualLineNum(tNode, lineNum);
     Assert::IsTrue(tNode->getChildren() == nullptr);
 
     ConstantNode *constNode = (ConstantNode *) tNode;
@@ -31,8 +31,8 @@ public:
     int lineNum = 5;
     std::string varName = "penguin";
     TNode *tNode = new VariableNode(lineNum, varName);
-    Assert::IsTrue(tNode->getType() == TNode::Type::Variable);
-    Assert::IsTrue(tNode->getLineNum() == lineNum);
+    isEqualType(tNode, TNode::Type::Variable);
+    isEqualLineNum(tNode, lineNum);
 
     VariableNode *varNode = (VariableNode *) tNode;
     Assert::IsTrue(varNode->getVarName() == varName);
@@ -57,7 +57,8 @@ public:
     Logger::WriteMessage("Running ASTBuilder test");
     ASTBuilder *builder = new ASTBuilder();
     AST *ast = builder->createAST();
-    Assert::IsTrue(ast->getRoot()->getType() == TNode::Type::Procedure);  // Check root initialized properly
+    isEqualType(ast->getRoot(), TNode::Type::Procedure);  // Check root initialized properly
+    isEqualLineNum(ast->getRoot(), TNode::NO_LINE_NUM);
 
     TNode *slNode = builder->createStmtList();
     // Check parent and child linked properly
@@ -83,6 +84,20 @@ public:
     AssignNode *assignNodeCopy = (AssignNode *)assignNode;
     Assert::IsTrue(assignNodeCopy->getLeftChild() == varNode);
     Assert::IsTrue(assignNodeCopy->getRightChild() == constNode);
+  }
+
+private:
+
+  /*  Given a TNode, checks if its type is equal to given type
+  */
+  void isEqualType(TNode * t_tNode, TNode::Type t_type) {
+    Assert::IsTrue(t_tNode->getType() == t_type);
+  }
+
+  /*  Given a TNode, checks if its line number is equal to given line number
+  */
+  void isEqualLineNum(TNode * t_tNode, int t_lineNum) {
+    Assert::IsTrue(t_tNode->getLineNum() == t_lineNum);
   }
 
   };
