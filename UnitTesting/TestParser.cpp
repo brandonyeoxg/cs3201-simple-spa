b229b7b6959a738bbf19b3eae467f22e6148e524
brandonyeoxg <brandonyeoxg@gmail.com>
2017-09-03 00:10:23 +0800

    Add test cases for exception handling of parser

--- UnitTesting/TestParser.cpp
+++ UnitTesting/TestParser.cpp
@@ -1,7 +1,11 @@
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <functional>
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "Parser.h"
-#include <cstdio>
+#include "SyntaxErrorException.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,16 +15,53 @@ namespace UnitTesting
   {
   public:
 
-    TEST_METHOD(parse)
+    TEST_METHOD(parseValidProgram)
     {
       PKB pkb;
       Parser parser(&pkb);
-      int actual = parser.parse("../simple-prog.txt");
+      int actual = parser.parse("../AutoTester/simple-prog.txt");
       int expected = 0;
       Assert::AreEqual(expected, actual);
 
       actual = parser.parse("randomTXT.txt");
       Assert::AreNotEqual(expected, actual);
+    }
+
+    TEST_METHOD(parseProgramWithNoOpenBraceThrowException) 
+    {
+      assertParseOnProgramExpectException("procedure test } \n");
+    }
+
+    TEST_METHOD(parseProgramWithNoSemiColonThrowException)
+    {
+      assertParseOnProgramExpectException("procedure test {x=y} \n");
+    }
+
+    TEST_METHOD(parseProgramWithNoOperatorThrowException)
+    {
+      assertParseOnProgramExpectException("procedure test {xy;} \n");
+    }
+
+    TEST_METHOD(parseProgramWithIncorrectCommandThrowException)
+    {
+      assertParseOnProgramExpectException("procedure test{x=y;whi i {z=y;}} \n");
+    }
+
+    TEST_METHOD(parseProgramWithNoCloseBraceThrowException)
+    {
+      assertParseOnProgramExpectException("procedure test{x=y; \n");
+      assertParseOnProgramExpectException("procedure test{x=y; \n\n");
+    }
+
+    void assertParseOnProgramExpectException(const std::string &t_programString) {
+      PKB pkb;
+      Parser *parser = new Parser(&pkb);
+      ofstream tmpFile;
+      const std::string tmpFileName = "testSimple.txt";
+      tmpFile.open(tmpFileName);
+      tmpFile << t_programString;
+      tmpFile.close();
+      auto funcPtr = [parser, tmpFileName] { parser->parse(tmpFileName); };
     }
   };
 }
