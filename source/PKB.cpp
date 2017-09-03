4b3f755711a8c7b692319b94ed1e372a17a4e4b3
pengcheng229 <pengcheng@hive-up.com>
2017-09-03 17:18:34 +0800

    PKB: create constructor, instantiate 3 tables.

--- source/PKB.cpp
+++ source/PKB.cpp
@@ -10,6 +10,15 @@ using namespace std;
 #include "PKB.h"
 #include "TNode.h"
 
+/**
+* A constructor.
+* Instantiates one VarTable, FollowTable, ParentTable each.
+*/
+PKB::PKB() {
+  FollowTable *followTable = new FollowTable();
+  ParentTable *parentTable = new ParentTable();
+  VarTable *varTable = new VarTable();
+}
 int PKB::setProcToAST(PROC p, TNode* r) {
   return NULL;
 }
--- source/PKB.h
+++ source/PKB.h
@@ -3,19 +3,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "FollowTable.h"
+#include "ParentTable.h"
+#include "VarTable.h"
 
 using namespace std;
 typedef short PROC;
 
+
+
 class TNode;
 
-class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
+//class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
 class PKB {
 public:
-  static VarTable* varTable;
   static int setProcToAST(PROC p, TNode* r);
   static TNode* getRootAST(PROC p);
+  PKB();
 private:
 
 };
