8ed7ba6c1c999a5c316cd062cd301b71b03117ec
pengcheng229 <pengcheng@hive-up.com>
2017-09-03 20:55:58 +0800

    comments for PKB. create returnParentTable method

--- source/PKB.cpp
+++ source/PKB.cpp
@@ -20,6 +20,7 @@ PKB::PKB() {
   m_varTable = new VarTable();
 }
 
+/* Getter methods*/
 FollowTable* PKB:: getFollowTable() {
   return m_followTable;
 }
@@ -32,34 +33,71 @@ VarTable* PKB::getVarTable() {
   return m_varTable;
 }
 
+/**
+* Method that returns an unordered_map for follow relationship.
+* @param s1 an integer argument (-1 denotes it being the querying variable).
+* @param s2 an integer argument (-2 denotes it being the querying variable).
+*/
 unordered_map<int, vector<int>> PKB:: returnFollowTable(int s1, int s2) {
-  FollowTable* testFollowTable = getFollowTable();
+  //obtain the follow table ptr
+  FollowTable* table = getFollowTable();
   
   //check s1 and s2
   //if both are querying variables, return the whole followTable i.e. every possible follow relationships
   if (s1 == VARIABLE_S1 && s2 == VARIABLE_S2) {
-    return testFollowTable->getFollowTable();
+    //if both input are querying variable, just return the entire map.
+    return table->getFollowTable();
   }
 
   unordered_map<int, vector<int>> finalResult;
   //if only s1 is querying variable
   if (s1 == VARIABLE_S1) {
-    vector<int> s1Results = testFollowTable->getS1(s2);
+    vector<int> s1Results = table->getS1(s2);
     finalResult.emplace(s2, s1Results);
   }
   else if (s2 == VARIABLE_S2) {
-    vector<int> s2Results = testFollowTable->getS2(s1);
+    vector<int> s2Results = table->getS2(s1);
     finalResult.emplace(s1, s2Results);
   }
 
   return finalResult;
 }
 
+/**
+* Method that returns an unordered_map for parent relationship.
+* @param s1 an integer argument (-1 denotes it being the querying variable).
+* @param s2 an integer argument (-2 denotes it being the querying variable).
+*/
+unordered_map<int, vector<int>> PKB::returnParentTable(int s1, int s2) {
+  ParentTable* table = getParentTable();
 
+  //check s1 and s2
+  //if both are querying variables, return the whole followTable i.e. every possible follow relationships
+  if (s1 == VARIABLE_S1 && s2 == VARIABLE_S2) {
+    //if both input are querying variable, just return the entire map.
+    return table->getParentTable();
+  }
+
+  unordered_map<int, vector<int>> finalResult;
+  //if only s1 is querying variable
+  if (s1 == VARIABLE_S1) {
+    vector<int> s1Results = table->getS1(s2);
+    finalResult.emplace(s2, s1Results);
+  }
+  else if (s2 == VARIABLE_S2) {
+    vector<int> s2Results = table->getS2(s1);
+    finalResult.emplace(s1, s2Results);
+  }
+
+  return finalResult;
+}
+
+//TBD
 int PKB::setProcToAST(PROC p, TNode* r) {
   return NULL;
 }
 
+//TBD
 TNode* PKB::getRootAST(PROC p) {
   return NULL;
 }
