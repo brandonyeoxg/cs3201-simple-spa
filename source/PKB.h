ff50b59f7782357176dfe4d80402264af893b1d2
pengcheng229 <pengcheng@hive-up.com>
2017-09-03 17:33:06 +0800

    PKB: create methods to get the tables based on querying parameters. create const to determine the variable in the method.

--- source/PKB.h
+++ source/PKB.h
@@ -3,13 +3,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include "FollowTable.h"
 #include "ParentTable.h"
 #include "VarTable.h"
 
 using namespace std;
 typedef short PROC;
-
+const int VARIABLE_S1 = -1;
+const int VARIABLE_S2 = -2;
+const string VARIABLE = "VARIABLE";
 
 
 class TNode;
@@ -21,6 +24,10 @@ public:
   static int setProcToAST(PROC p, TNode* r);
   static TNode* getRootAST(PROC p);
   PKB();
+  unordered_map<int, vector<int>> getFollowTable(int s1, int s2);
+  unordered_map<int, vector<int>> getParentTable(int s1, int s2);
+  unordered_map<string, vector<int>> getVarTable(string var);
+
 private:
 
 };
