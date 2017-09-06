8949a0b0d68fb5478ffa4fbea00bfc10b55ddaa9
brandonyeoxg <brandonyeoxg@gmail.com>
2017-09-06 18:13:53 +0800

    Add helper methods to insert into the procedure table through PKB.

--- source/PKB.h
+++ source/PKB.h
@@ -4,36 +4,39 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include "ProcTable.h"
 #include "FollowTable.h"
 #include "ParentTable.h"
 #include "VarTable.h"
+#include "AST.h"
+#include "ASTBuilder.h"
+#include "nodes\TNode.h"
+#include "nodes\ProcedureNode.h"
 
-typedef short PROC;
+typedef short PROC_INDEX_NO;
 const int VARIABLE_S1 = -1;
 const int VARIABLE_S2 = -2;
 
-
-class TNode;
-
-//class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
-
 class PKB {
 public:
-  int setProcToAST(PROC p, TNode* r);
-  TNode* getRootAST(PROC p);
   PKB();
+  ~PKB();
   FollowTable* getFollowTable();
   ParentTable* getParentTable();
   VarTable* getVarTable();
   std::unordered_map<int, std::vector<int>> returnFollowTable(int s1, int s2);
   std::unordered_map<int, std::vector<int>> returnParentTable(int s1, int s2);
   std::unordered_map<std::string, std::vector<int>> returnVarTable(std::string var);
-
+  
+  PROC_INDEX_NO insertProc(ProcedureNode* t_node);
+  int setProcToAST(PROC_INDEX_NO t_index, ProcedureNode* t_node);
+  TNode* getRootAST(PROC_INDEX_NO t_index);
 private:
   FollowTable* m_followTable;
   ParentTable* m_parentTable;
   VarTable* m_varTable;
+  ProcTable* m_procTable;
 
-  
-  TNode* m_ast; /*< Brandon stuff */
+  ASTBuilder m_builder;
+  AST m_programAst; /*< Brandon stuff */
 };
