0b46cd5db082bff0151c5d0ee1a27509a3ecd203
turtle96 <angjazlyn@hotmail.com>
2017-09-08 00:04:34 +0800

    ah forgot this file

--- source/ASTUtilities.h
+++ source/ASTUtilities.h
@@ -17,6 +17,7 @@ private:
   static std::string getStringFromNode(TNode *t_node);
   static std::string convertTreeToString(TNode *t_node);
   static bool isNodeAnOperator(TNode * t_node);
+  static std::vector<std::string> addStrIfNotDuplicate(std::vector<std::string> t_listOfStr, std::string t_str);
   static std::vector<std::string> generateStringList(TwoChildrenNode *t_node, std::vector<std::string> t_listOfStr);
   //static std::vector<std::string> generateStrings(TwoChildrenNode *t_node);
 };
