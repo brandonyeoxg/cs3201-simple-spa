cada643d861d71278f55dfe05f5d2c17ba4fad12
brandonyeoxg <brandonyeoxg@gmail.com>
2017-09-07 20:09:46 +0800

    Implement procTable into PKB.

--- source/PKB.h
+++ source/PKB.h
@@ -9,8 +9,8 @@
 #include "ParentTable.h"
 #include "VarTable.h"
 #include "ASTBuilder.h"
+#include "ProcTable.h"
 
-typedef short PROC;
 const int VARIABLE_S1 = -1;
 const int VARIABLE_S2 = -2;
 
@@ -28,12 +28,14 @@ public:
   std::unordered_map<int, std::vector<int>> returnFollowTable(int s1, int s2);
   std::unordered_map<int, std::vector<int>> returnParentTable(int s1, int s2);
   std::unordered_map<std::string, std::vector<int>> returnVarTable(std::string var);
-  int setProcToAST(PROC p, TNode* r);
-  TNode* getRootAST(PROC p);
+
+  PROC_INDEX_NO insertProcToAST(ProcedureNode* t_node);
+  ProcedureNode* getRootAST(PROC_INDEX_NO t_index);
 private:
   FollowTable* m_followTable;
   ParentTable* m_parentTable;
   VarTable* m_varTable;
+  ProcTable* m_procTable;
 
   AST m_programNode;
   ASTBuilder m_builder;
