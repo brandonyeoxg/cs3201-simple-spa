c316d8ba00ab1e42b59b7668b94b5a01e634d034
brandonyeoxg <brandonyeoxg@gmail.com>
2017-09-09 03:22:47 +0800

    Implement AssignTable, need to optimise this some more, badly implemented!

--- source/AssignTable.cpp
+++ source/AssignTable.cpp
@@ -1,28 +1,59 @@
 #include "AssignTable.h"
+#include "nodes\VariableNode.h"
 
 VAR_INDEX_NO AssignTable::insertAssignRelation(const VAR_INDEX_NO& t_index, AssignNode* t_node) {
   // Check of the variable has already been added
-  std::unordered_map<VAR_INDEX_NO, std::list<AssignNode*>>::iterator itr = m_data.find(t_index);
+  std::unordered_map<VAR_INDEX_NO, std::list<AssignData>>::iterator itr = m_data.find(t_index);
   if (itr != m_data.end()) {
     // Append
-    (*itr).second.push_back(t_node);
+    AssignData assignData(t_node, (STMT_NO)t_node->getLineNum());
+    (*itr).second.push_back(assignData);
     return t_index;
   }
-  std::list<AssignNode*> nodeList;
-  nodeList.push_back(t_node);
-  m_data.insert({ t_index, nodeList });
-
+  AssignData assignNodeData(t_node, (STMT_NO)t_node->getLineNum());
+  std::list<AssignData> assignList;
+  assignList.push_back(assignNodeData);
+  m_data.insert({ t_index, assignList });
   return t_index;
 }
 
 std::list<STMT_NO> AssignTable::getAllStmtListByVar(VAR_INDEX_NO t_index) {
-  (*m_data.find(t_index)).second;
+  std::list<STMT_NO> output;
+  std::unordered_map<VAR_INDEX_NO, std::list<AssignData>>::iterator itr = m_data.find(t_index);
+  if (itr == m_data.end()) {
+    return output;
+  }
+  std::list<AssignData>::iterator listItr = (*itr).second.begin();
+  for (; listItr != (*itr).second.end(); listItr++) {
+    output.push_back((*listItr).m_assignStmt);
+  }
+  return output;
 }
 
 std::list<STMT_NO> AssignTable::getAllStmtList() {
-
+  std::list<STMT_NO> output;
+  std::unordered_map<VAR_INDEX_NO, std::list<AssignData>>::iterator itr = m_data.begin();
+  for (; itr != m_data.end(); itr++) {
+    std::list<AssignData>::iterator listItr = (*itr).second.begin();
+    for (; listItr != (*itr).second.end(); listItr++) {
+      output.push_back((*listItr).m_assignStmt);
+    }
+  }
+  return output;
 }
 
-std::unordered_map<VAR_INDEX_NO, std::list<STMT_NO>> AssignTable::getAllAssignStmtWithVar() {
-
+std::unordered_map<std::string, std::list<STMT_NO>> AssignTable::getAllAssignStmtWithVar() {
+  std::unordered_map<std::string, std::list<STMT_NO>> output;
+  std::unordered_map<VAR_INDEX_NO, std::list<AssignData>>::iterator itr = m_data.begin();
+  for (; itr != m_data.end(); itr++) {
+    std::list<AssignData> startList = (*itr).second;
+    std::list<AssignData>::iterator listItr = startList.begin();
+    std::list<STMT_NO> outputStmtNo;
+    std::string varName = ((VariableNode*)((*listItr).m_assignNode->getLeftChild()))->getVarName();
+    for (; listItr != startList.end(); listItr++) {
+      outputStmtNo.push_back((*listItr).m_assignStmt);
+    }
+    output.insert({ varName, outputStmtNo });
+  }
+  return output;
 }
--- source/AssignTable.h
+++ source/AssignTable.h
@@ -5,6 +5,12 @@
 typedef int VAR_INDEX_NO;
 typedef int STMT_NO;
 
+struct AssignData {
+  AssignNode* m_assignNode;
+  STMT_NO m_assignStmt;
+  AssignData(AssignNode* t_node, STMT_NO t_stmt) : m_assignNode(t_node) , m_assignStmt(t_stmt) {}
+};
+
 /**
  * Represents the assignments made in the program in a table.
  *
@@ -16,7 +22,7 @@ public:
   VAR_INDEX_NO insertAssignRelation(const VAR_INDEX_NO &t_index, AssignNode* t_node);
   std::list<STMT_NO> getAllStmtListByVar(VAR_INDEX_NO t_index);
   std::list<STMT_NO> getAllStmtList();
-  std::unordered_map<VAR_INDEX_NO, std::list<STMT_NO>> getAllAssignStmtWithVar();
+  std::unordered_map<std::string, std::list<STMT_NO>> getAllAssignStmtWithVar();
 private:
-  std::unordered_map<VAR_INDEX_NO, std::list<AssignNode*>> m_data;
+  std::unordered_map<VAR_INDEX_NO, std::list<AssignData>> m_data;
 };
