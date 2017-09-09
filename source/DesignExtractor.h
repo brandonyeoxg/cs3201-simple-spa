b1cf4470362b2ef85b3880ec3d6c7bc8d1870994
brandonyeoxg <brandonyeoxg@gmail.com>
2017-09-09 20:53:20 +0800

    Implment design extractor to extract out parentStar abstraction.

--- source/DesignExtractor.h
+++ source/DesignExtractor.h
@@ -1,17 +1,26 @@
 #pragma once
-
+#include "ParentTable.h"
+#include "PKB.h"
 /**
  * Represents an extractor which extract complicated abstractions from AST.
  *
  * @author Brandon
  * @date 8/27/2017
  */
-
-class ParentsTable;
-class FollowsTable;
-
 class DesignExtractor {
 public:
-  ParentsTable* obtainParentsStar(TNode *t_astRoot);
-  FollowsTable* obtainFollowsStar(TNode *t_astRoot);
+  DesignExtractor(PKB* t_pkb);
+  void extractRestOfDesignAbstractions();
+
+private:
+  void extractParentStarAbstraction();
+  void extractParentStarMap(
+    std::unordered_map<int, int>::iterator t_mapItr,
+    std::unordered_map<int, int>& t_parentMap,
+    std::unordered_map<int, std::list<std::list<int>>>& t_parentStarMap);
+  void extractParentedByStarMap(
+    std::unordered_map<int, int>::iterator t_mapItr,
+    std::unordered_map<int, int>& t_parentMap,
+    std::unordered_map<int, std::list<int>>& t_parentedByStarMap);
+  PKB* m_pkb;
 };
