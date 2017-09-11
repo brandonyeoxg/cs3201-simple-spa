b2ff75b6b02557d49bda27752f2debb81df3a61b
brandonyeoxg <brandonyeoxg@gmail.com>
2017-09-12 00:32:16 +0800

    Implement ProcTable with uses and modifies.

--- source/ProcTable.h
+++ source/ProcTable.h
@@ -1,8 +1,14 @@
 #pragma once
 #include <vector>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
 #include "nodes\ProcedureNode.h"
+#include "ProcTableData.h"
 
 typedef short PROC_INDEX_NO;
+typedef int VAR_INDEX_NO;
+const PROC_INDEX_NO INVALID_PROC_NO = -1;
 
 /**
  * Represents a procedure table.
@@ -27,7 +33,49 @@ public:
   * @param t_node should not be null
   * @return the index position of the node
   */
-  ProcedureNode* getProcWithIndex(PROC_INDEX_NO t_index);
+  ProcTableData* getProcTableDataWithName(std::string& procName);
+
+  /**
+   * Gets procedure index number by using the procedure name.
+   *
+   * @author Brandon
+   * @date 11/9/2017
+   *
+   * @param [in] procName Name of the proc.
+   *
+   * @return The proc index number with name.
+   */
+  PROC_INDEX_NO getProcIdxNumWithName(std::string& procName);
+
+  ProcedureNode* getProcNodeWithIdx(PROC_INDEX_NO& t_procIdx);
+
+  std::string getProcNameWithIdx(PROC_INDEX_NO& t_procIdx);
+
+  bool insertModifies(PROC_INDEX_NO& t_procIdx, std::string& t_varIdx);
+  bool isModifies(PROC_INDEX_NO& t_procIdx, std::string& t_varName);
+  std::list<std::string>& getProcOfVarModifies(PROC_INDEX_NO& t_procIdx);
+  std::list<std::string>& getProcNameThatModifiesVar(std::string& t_varName);
+  std::unordered_map<std::string, std::list<std::string>>& getProcAndVarModifies();
+  bool isModifiesInProc(std::string& t_procName);
+  std::list<std::string>& getProcNameThatModifies();
+
+  bool insertUses(PROC_INDEX_NO& t_procIdx, std::string& t_varIdx);
+  bool isUses(PROC_INDEX_NO& t_procIdx, std::string& t_varName);
+  std::list<std::string>& getProcOfVarUses(PROC_INDEX_NO& t_procIdx);
+  std::list<std::string>& getProcNameThatUsesVar(std::string& t_varName);
+  std::unordered_map<std::string, std::list<std::string>>& getProcAndVarUses();
+  bool isUsesInProc(std::string& t_procName);
+  std::list<std::string>& getProcNameThatUses();
+
+  void convertProcTableSetToList();
 private:
-  std::vector<std::pair<std::string, ProcedureNode*>> m_data;
+  std::vector<ProcTableData> m_data;
+
+  std::unordered_map<std::string, std::list<std::string>> m_varToProcLookUpForModifies;
+  std::unordered_map<std::string, std::list<std::string>> m_varToProcLookUpForUses;
+  std::unordered_map<std::string, std::list<std::string>> m_procWithVarNamesModifies;
+  std::unordered_map<std::string, std::list<std::string>> m_procWithVarNamesUses;
+
+  std::list<std::string> m_procThatModifies;
+  std::list<std::string> m_procThatUses;
 };
