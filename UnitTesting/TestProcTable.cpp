8617d66e08a7e0254a5add9cc21aaa5a57432b4b
brandonyeoxg <brandonyeoxg@gmail.com>
2017-09-12 14:11:28 +0800

    Add ProcTable unit testing.

--- UnitTesting/TestProcTable.cpp
+++ UnitTesting/TestProcTable.cpp
@@ -59,6 +59,54 @@ namespace UnitTesting {
       Assert::IsFalse(procTable.isModifies(dummyProcName, std::string("BYEBYE")));
     }
 
+    TEST_METHOD(getVarFromProcModifies)
+    {
+      ASTBuilder builder;
+      std::string dummyProcName = "dummy";
+      ProcedureNode* dummyProc = builder.createProcedure(dummyProcName);
+      ProcTable procTable;
+      PROC_INDEX_NO index = procTable.insertProcByProcNode(dummyProc);
+      std::string varName1 = "HELLO";
+      std::string varName2 = "BYE";
+      procTable.insertModifies(index, varName1);
+      procTable.insertModifies(index, varName2);
+
+      procTable.convertProcTableSetToList();
+
+      std::list<std::string> actual = procTable.getVarFromProcModifies(index);
+      std::list<std::string> expected = {varName2, varName1};
+
+      Assert::IsTrue(actual == expected);
+    }
+
+    TEST_METHOD(getProcNameThatModifiesVar) 
+    {
+      ASTBuilder builder;
+      std::string dummyProcName = "dummy";
+      ProcedureNode* dummyProc = builder.createProcedure(dummyProcName);
+      ProcTable procTable;
+      PROC_INDEX_NO index = procTable.insertProcByProcNode(dummyProc);
+      std::string varName1 = "HELLO";
+      std::string varName2 = "BYE";
+
+      procTable.insertModifies(index, varName1);
+      procTable.insertModifies(index, varName2);
+
+      dummyProcName = dummyProcName + "1";
+      dummyProc = builder.createProcedure(dummyProcName);
+      index = procTable.insertProcByProcNode(dummyProc);
+      procTable.insertModifies(index, varName2);
+      procTable.convertProcTableSetToList();
+
+      std::list<std::string> expected = { "dummy" };
+      std::list<std::string> actual = procTable.getProcNameThatModifiesVar(varName1);
+      Assert::IsTrue(expected == actual);
+      actual = procTable.getProcNameThatModifiesVar(varName2);
+      expected = { "dummy", "dummy1" };
+      Assert::IsTrue(expected == actual);
+    }
+
+
     TEST_METHOD(isUses)
     {
       ASTBuilder builder;
@@ -72,6 +120,26 @@ namespace UnitTesting {
 
       Assert::IsTrue(procTable.isUses(dummyProcName, std::string("HELLO")));
       Assert::IsFalse(procTable.isUses(dummyProcName, std::string("BYEBYE")));
+    }
+
+    TEST_METHOD(getVarFromProcUses)
+    {
+      ASTBuilder builder;
+      std::string dummyProcName = "dummy";
+      ProcedureNode* dummyProc = builder.createProcedure(dummyProcName);
+      ProcTable procTable;
+      PROC_INDEX_NO index = procTable.insertProcByProcNode(dummyProc);
+      std::string varName1 = "HELLO";
+      std::string varName2 = "BYE";
+      procTable.insertUses(index, varName1);
+      procTable.insertUses(index, varName2);
+
+      procTable.convertProcTableSetToList();
+
+      std::list<std::string> actual = procTable.getVarFromProcUses(index);
+      std::list<std::string> expected = { varName2, varName1 };
+
+      Assert::IsTrue(actual == expected);
     }
   };
 }
