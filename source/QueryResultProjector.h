6570a20d2c79dd65261e9f1bf8eec35a9da5ce25
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2017-09-14 19:28:58 +0800

    Fixed/Generated comments for Grammar, QueryResultProjector and Relation header files, for generating Doxygen documentation.

--- source/QueryResultProjector.h
+++ source/QueryResultProjector.h
@@ -5,10 +5,18 @@
 #include <list>
 #include <vector>
 
+/**
+*  A class used to format the output string vector into a string list.
+*/
 class QueryResultProjector
 {
 public:
-  std::list<std::string> formatResult(std::vector<std::string>);
+  /**
+  * Formats the string vector into a list string.
+  * @params result is a vector<string> from the QueryEvaluator.
+  * @return The same result but as a list<string> instead.
+  */
+  std::list<std::string> formatResult(std::vector<std::string> result);
 };
 
 #endif
