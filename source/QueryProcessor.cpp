d457cb5dee6c5f98dccb5dfc55b6f497844a2409
DESKTOP-M3U9JB8\aaron <a0123977@u.nus.edu>
2017-09-16 22:05:21 +0800

    209 out of 209 test cases passed
    removed bug regarding 2 underscores as parameters for design abstraction
    added unit testing for queryprocessor
    removed condition for pattern right side _ causing it to return false

--- source/QueryProcessor.cpp
+++ source/QueryProcessor.cpp
@@ -8,25 +8,29 @@ std::list<std::string> QueryProcessor::runQueryProcessor(std::string t_stringInp
   std::string result;
   std::list<std::string> resultList;
   std::vector<std::string> evaluatedResults;
-
+  bool isTokenized;
 
   declaration = qpp.splitStringDeclaration(t_stringInput);
-  qpp.tokenizeDeclaration(declaration);
-  query = qpp.splitStringQuery(t_stringInput);
-  qpp.tokenizeQuery(query);
+  isTokenized = qpp.tokenizeDeclaration(declaration);
 
+  query = qpp.splitStringQuery(t_stringInput);
+  isTokenized = qpp.tokenizeQuery(query);
+ 
   std::queue<Grammar> selectQueue = qpp.getSelect();
   std::queue<Relation> suchThatQueue = qpp.getSuchThat();
   std::queue<Pattern> patternQueue = qpp.getPattern();
   std::unordered_map<std::string, int> unorderedMap = qpp.getSynonym();
 
-  Grammar testGrammar = selectQueue.front();
-  //std::cout << "This is QueryProcessor testing selectQueue output: " << testGrammar.getName() << std::endl;
-  QueryEvaluator *qe = new QueryEvaluator(m_pkb, selectQueue, suchThatQueue, patternQueue, unorderedMap);
-  evaluatedResults = qe->evaluateQuery();
+  if (isTokenized == true) {
 
-  QueryResultProjector qrp;
-  resultList = qrp.formatResult(evaluatedResults);
-  
+    //Grammar testGrammar = selectQueue.front();
+    //std::cout << "This is QueryProcessor testing selectQueue output: " << testGrammar.getName() << std::endl;
+    QueryEvaluator *qe = new QueryEvaluator(m_pkb, selectQueue, suchThatQueue, patternQueue, unorderedMap);
+    evaluatedResults = qe->evaluateQuery();
+
+    QueryResultProjector qrp;
+    resultList = qrp.formatResult(evaluatedResults);
+  }
+
   return resultList;
 }
