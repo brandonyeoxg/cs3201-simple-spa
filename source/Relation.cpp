72342edd42ea44733dc201409232c1333a90d00f
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2017-09-17 17:15:28 +0800

    Refactored floating strings into static strings

--- source/Relation.cpp
+++ source/Relation.cpp
@@ -3,27 +3,35 @@
 #include "Grammar.h"
 
 Relation::Relation() {}
+
+std::string Relation::FLS = "Follows";
+std::string Relation::FLS_ = "Follows*";
+std::string Relation::PRT = "Parent";
+std::string Relation::PRT_ = "Parent*";
+std::string Relation::USE = "Uses";
+std::string Relation::MDF = "Modifies";
+
 Relation::RType Relation::getType() { return m_type; }
 Grammar Relation::getG1() { return m_g1; }
 Grammar Relation::getG2() { return m_g2; }
 
 Relation::Relation(std::string t_type, Grammar t_g1, Grammar t_g2) {
-  if (t_type.compare("Follows") == 0) {
+  if (t_type.compare(FLS) == 0) {
     m_type = RType::FOLLOWS;
   }
-  else if (t_type.compare("Follows*") == 0) {
+  else if (t_type.compare(FLS_) == 0) {
     m_type = RType::FOLLOWS_;
   }
-  else if (t_type.compare("Parent") == 0) {
+  else if (t_type.compare(PRT) == 0) {
     m_type = RType::PARENT;
   }
-  else if (t_type.compare("Parent*") == 0) {
+  else if (t_type.compare(PRT_) == 0) {
     m_type = RType::PARENT_;
   }
-  else if (t_type.compare("Uses") == 0) {
+  else if (t_type.compare(USE) == 0) {
     m_type = RType::USES;
   }
-  else if (t_type.compare("Modifies") == 0) {
+  else if (t_type.compare(MDF) == 0) {
     m_type = RType::MODIFIES;
   }
   m_g1 = t_g1;
