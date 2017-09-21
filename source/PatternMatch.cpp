a77c6cbdcc0e6029af89297160cbb58ba100e82e
turtle96 <angjazlyn@hotmail.com>
2017-09-22 01:09:08 +0800

    implement subtree strings generator

--- source/PatternMatch.cpp
+++ source/PatternMatch.cpp
@@ -25,6 +25,71 @@ PatternMatch::PatternMatch() {
 
 PatternMatch::~PatternMatch() {}
 
+//TODO needs a wrapper function, add the original string as well
+std::vector<std::string> PatternMatch::generateSubtreeStrings(std::vector<std::string> t_tokens, 
+  std::vector<std::string> t_subtreeStrings, int t_startIndex, int t_endIndex) {
+  if (t_startIndex == t_endIndex) {
+    return t_subtreeStrings;
+  }
+
+  int indexOfOperator = INVALID_INDEX;
+  int lastPlusOrMinus = INVALID_INDEX;
+  int lastMultiplyOrDivide = INVALID_INDEX;
+
+  for (int i = t_startIndex; i < t_endIndex; i++) {
+    if (t_tokens.at(i) == OPERATOR_PLUS || t_tokens.at(i) == OPERATOR_MINUS) {
+      lastPlusOrMinus = i;
+      break;
+    }
+
+    if (t_tokens.at(i) == OPERATOR_MULTIPLY || t_tokens.at(i) == OPERATOR_DIVIDE) {
+      lastMultiplyOrDivide = i;
+      break;
+    }
+  }
+
+  if (lastPlusOrMinus != INVALID_INDEX) {
+    indexOfOperator = lastPlusOrMinus;
+  } else if (lastMultiplyOrDivide != INVALID_INDEX) {
+    indexOfOperator = lastMultiplyOrDivide;
+  } else {  // no more operators
+    return t_subtreeStrings;
+  }
+
+  t_subtreeStrings = addStrIfNotDuplicate(t_subtreeStrings, convertVectorToStringWithIndex(t_tokens, t_startIndex, indexOfOperator - 1));
+  t_subtreeStrings = addStrIfNotDuplicate(t_subtreeStrings, convertVectorToStringWithIndex(t_tokens, indexOfOperator + 1, t_endIndex));
+
+  t_subtreeStrings = generateSubtreeStrings(t_tokens, t_subtreeStrings, t_startIndex, indexOfOperator - 1);
+  t_subtreeStrings = generateSubtreeStrings(t_tokens, t_subtreeStrings, indexOfOperator + 1, t_endIndex);
+
+  return t_subtreeStrings;
+}
+
+/** Given a vector of strings and start and end index, converts to a single string */
+std::string PatternMatch::convertVectorToStringWithIndex(std::vector<std::string> t_vector, int t_startIndex, int t_endIndex) {
+  std::string str = "";
+  
+  for (int i = t_startIndex; i < t_endIndex; i++) {
+    str += t_vector.at(i);
+  }
+
+  return str;
+}
+
+// Add string to list of strings only if it is not duplicate string
+std::vector<std::string> PatternMatch::addStrIfNotDuplicate(std::vector<std::string> t_listOfStr, std::string t_str) {
+
+  for (int i = 0; i < (int)t_listOfStr.size(); i++) {
+    if (t_listOfStr.at(i) == t_str) {
+      return t_listOfStr;
+    }
+  }
+
+  t_listOfStr.push_back(t_str);
+
+  return t_listOfStr;
+}
+
 /** Helper function to remove all whitespaces in a given string */
 std::string PatternMatch::removeWhitespaces(std::string t_str) {
   t_str.erase(std::remove(t_str.begin(), t_str.end(), ' '), t_str.end());
--- source/PatternMatch.h
+++ source/PatternMatch.h
@@ -21,5 +21,8 @@ private:
   std::unordered_map<STMT_NUM, string> assignStmts;  /**< String representation of all assignment statements (right-hand side of equal sign) mapped to statement numbers. */ 
   std::unordered_map<STMT_NUM, std::vector<string>> assignStmtsSubtrees;  /**< Vector of all subtree strings mapped to statement number */
   
+  std::vector<std::string> generateSubtreeStrings(std::vector<std::string> t_tokens, std::vector<std::string> t_subtreeStrings, int t_startIndex, int t_endIndex);
+  std::string convertVectorToStringWithIndex(std::vector<std::string> t_vector, int t_startIndex, int t_endIndex);
+  std::vector<std::string> addStrIfNotDuplicate(std::vector<std::string> t_listOfStr, std::string t_str);
   std::string removeWhitespaces(std::string t_str);
 };
