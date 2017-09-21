8176d3e05fa0a156b7ff32f7ba3bbb2dd7149fd8
turtle96 <angjazlyn@hotmail.com>
2017-09-21 23:44:12 +0800

    add singleton pattern, implement add assign statement

--- source/PatternMatch.cpp
+++ source/PatternMatch.cpp
@@ -1,4 +1,33 @@
 #include "PatternMatch.h"
 
-PatternMatch::PatternMatch() {
+PatternMatch PatternMatch::getInstance() {
+  if (&patternMatch == 0) {
+    patternMatch = PatternMatch();
+  }
+
+  return patternMatch;
 }
+
+void PatternMatch::addAssignStmt(STMT_NUM t_stmtNum, std::vector<std::string> t_stmtTokens) {
+  std::string stmtStr = "";
+  for (int i = 0; i < t_stmtTokens.size(); i++) {
+    t_stmtTokens.at(i) = removeWhitespaces(t_stmtTokens.at(i));
+    stmtStr += t_stmtTokens.at(i);
+  }
+
+  assignStmts.insert({ t_stmtNum, stmtStr });
+}
+
+PatternMatch::PatternMatch() {
+  assignStmts = std::unordered_map<STMT_NUM, string>();
+  assignStmtsSubtrees = std::unordered_map<STMT_NUM, std::vector<string>>();
+}
+
+PatternMatch::~PatternMatch() {}
+
+/** Helper function to remove all whitespaces in a given string */
+std::string PatternMatch::removeWhitespaces(std::string t_str) {
+  t_str.erase(std::remove(t_str.begin(), t_str.end(), ' '), t_str.end());
+  return t_str;
+}
+
--- source/PatternMatch.h
+++ source/PatternMatch.h
@@ -3,14 +3,23 @@
 #include <unordered_map>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 #include ".\GlobalTypeDef.h"
 
 class PatternMatch {
 public:
-  PatternMatch();
+  static PatternMatch getInstance();
+
+  void addAssignStmt(STMT_NUM t_stmtNum, std::vector<std::string> t_stmtTokens);
 
 private:
-  std::unordered_map<STMT_NUM, std::vector<string>> assignStmts;
-  std::unordered_map<STMT_NUM, std::vector<string>> assignStmtsSubtrees;
+  PatternMatch();
+  ~PatternMatch();
+
+  static PatternMatch patternMatch;
+  std::unordered_map<STMT_NUM, string> assignStmts;  /**< String representation of all assignment statements (right-hand side of equal sign) mapped to statement numbers. */ 
+  std::unordered_map<STMT_NUM, std::vector<string>> assignStmtsSubtrees;  /**< Vector of all subtree strings mapped to statement number */
+  
+  std::string removeWhitespaces(std::string t_str);
 };
