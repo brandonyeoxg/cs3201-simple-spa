507009a23e68b46935d7daa0f90701d4331e3bc1
peng229 <e0003632@u.nus.edu>
2017-09-25 22:55:34 +0800

    create followedByMap, to populate using insertFollows method.

--- source/FollowTable.cpp
+++ source/FollowTable.cpp
@@ -12,42 +12,59 @@
 #include "FollowTable.h"
 
 void FollowTable::setFollowTable(std::unordered_map<int, std::vector<int>> &t_table) {
-  m_followTable = t_table;
+  m_followMap = t_table;
 }
 
 std::unordered_map<int, std::vector<int>> FollowTable::getFollowTable() {
-  return m_followTable;
+  return m_followMap;
 }
 
 bool FollowTable::insertFollows(int t_s1, int t_s2) {
   if (t_s1 == 0) {
     return false;
   }
-  if (m_followTable.find(t_s1) == m_followTable.end()) {
-  //if s1 is not present in followTable
+
+  //insertion to followMap
+  if (m_followMap.find(t_s1) == m_followMap.end()) {
+  //if s1 is not present in followMap
     std::vector<int> lineNums;
     lineNums.push_back(t_s2);
-    m_followTable.emplace(t_s1, lineNums);
+    m_followMap.emplace(t_s1, lineNums);
     m_allFollows.insert(t_s2);
   } else {
     //if not, first check if the existing vector consists s2; if it does, return false
-    std::vector<int> lineNums = m_followTable[t_s1];
+    std::vector<int> lineNums = m_followMap[t_s1];
     if (std::find(lineNums.begin(), lineNums.end(), t_s2) != lineNums.end()) {
       return false;
     }
   }
-  //else, retrieve the existing vector, append, and put back to followTable.
+  //else, retrieve the existing vector, append, and put back to followMap.
   //also for every existing vector, check if s1 exists. If it does, append s2.
-  for (auto it = m_followTable.begin(); it != m_followTable.end(); ++it) {
+  for (auto it = m_followMap.begin(); it != m_followMap.end(); ++it) {
     std::vector<int> vect = it->second;
     for (int i = 0; i < vect.size(); i++) {
       if (vect[i] == t_s1) { //if s1 present in vector
         vect.push_back(t_s2);
-        //update followtable!
-        m_followTable[it->first] = vect;
+        //update followMap
+        m_followMap[it->first] = vect;
         m_allFollows.insert(t_s2);
       }
     }
+  }
+
+  //insertion to followedByMap
+  //firstly, check if s2 exists as key in followedByMap.
+  if (m_followedByMap.find(t_s2) == m_followedByMap.end()) {
+    //if it doesn't, simply emplace s1 as key.
+    std::vector<int> lineNums;
+    lineNums.push_back(t_s1);
+    m_followMap.emplace(t_s2, lineNums);
+  } else {
+    auto iterator = m_followedByMap.find(t_s1);
+    std::vector<int> lineNumsFollowedByS1 = iterator->second;
+    std::vector<int> lineNums;
+    lineNums.push_back(t_s1);
+    lineNums.insert(lineNums.end(), lineNumsFollowedByS1.begin(), lineNumsFollowedByS1.end());
   }
   return true;
 }
@@ -55,11 +72,11 @@ bool FollowTable::insertFollows(int t_s1, int t_s2) {
 bool FollowTable::isFollows(int t_s1, int t_s2) {
   //if s1 doesn't exist in followtable, returns false.
   //else, check if s2 is the first element in vector of s1.
-  if (m_followTable.find(t_s1) == m_followTable.end()) {
-    //if s1 is not present in followTable
+  if (m_followMap.find(t_s1) == m_followMap.end()) {
+    //if s1 is not present in followMap
     return false;
   } else {
-    std::vector<int> lineNums = m_followTable[t_s1];
+    std::vector<int> lineNums = m_followMap[t_s1];
     if (lineNums[0] == t_s2) {
       //is the first element in vector
       return true;
@@ -72,11 +89,11 @@ bool FollowTable::isFollows(int t_s1, int t_s2) {
 bool FollowTable::isFollowsStar(int t_s1, int t_s2) {
   //in this case, since s1 is known,
   //we just retrieve the vector mapped to s1 and check if s2 exists.
-  if (m_followTable.find(t_s1) == m_followTable.end()) {
-    //if s1 is not present in followTable
+  if (m_followMap.find(t_s1) == m_followMap.end()) {
+    //if s1 is not present in followMap
     return false;
   } else {
-    std::vector<int> lineNums = m_followTable[t_s1];
+    std::vector<int> lineNums = m_followMap[t_s1];
     if (std::find(lineNums.begin(), lineNums.end(), t_s2) != lineNums.end()) {
       //can be found in the vector
       return true;
@@ -89,17 +106,17 @@ bool FollowTable::isFollowsStar(int t_s1, int t_s2) {
 int FollowTable::getFollows(int t_s1) {
   //in this case, since s1 is known,
   //we just retrieve the vector mapped to s1 return the .
-  if (m_followTable.find(t_s1) == m_followTable.end()) {
-    //if s1 is not present in followTable, throw exception
+  if (m_followMap.find(t_s1) == m_followMap.end()) {
+    //if s1 is not present in followMap, throw exception
     throw std::invalid_argument("key s1 does not exist in FollowTable");
   } else {
-    std::vector<int> lineNums = m_followTable[t_s1];
+    std::vector<int> lineNums = m_followMap[t_s1];
     return lineNums[0];
   }
 }
 
 int FollowTable::getFollowedBy(int t_s2) {
-  for (auto it = m_followTable.begin(); it != m_followTable.end(); ++it) {
+  for (auto it = m_followMap.begin(); it != m_followMap.end(); ++it) {
     std::vector<int> vect = it->second;
     for (int i = 0; i < vect.size(); i++) {
       if (vect[i] == t_s2) { //if s2 present in vector
@@ -111,21 +128,21 @@ int FollowTable::getFollowedBy(int t_s2) {
       }
     }
   }
-  //if nothing is returned after the for loop terminates, throw invalid_argument exception (i.e. s2 does not exist in followTable).
+  //if nothing is returned after the for loop terminates, throw invalid_argument exception (i.e. s2 does not exist in followMap).
   throw std::invalid_argument("s2 does not exist in FollowTable");
 }
 
 std::vector<int> FollowTable::getFollowsStar(int t_s1) {
   std::vector<int> lineNums;
-  if (m_followTable.find(t_s1) != m_followTable.end()) {
-    lineNums = m_followTable[t_s1];
+  if (m_followMap.find(t_s1) != m_followMap.end()) {
+    lineNums = m_followMap[t_s1];
   }
   return lineNums;
 }
 
 std::vector<int> FollowTable::getFollowedByStar(int t_s2) {
   std::vector<int> result;
-  for (auto it = m_followTable.begin(); it != m_followTable.end(); it++) {
+  for (auto it = m_followMap.begin(); it != m_followMap.end(); it++) {
     std::vector<int> vect = it->second;
     if (std::find(vect.begin(), vect.end(), t_s2) != vect.end()) {
       int lineNum = it->first;
@@ -138,7 +155,7 @@ std::vector<int> FollowTable::getFollowedByStar(int t_s2) {
 
 std::unordered_map<int, int> FollowTable::getAllFollows() {
   std::unordered_map<int, int> allFollows;
-  for (auto it = m_followTable.begin(); it != m_followTable.end(); ++it) {
+  for (auto it = m_followMap.begin(); it != m_followMap.end(); ++it) {
     int lineNum = it->first;
     std::vector<int> vect = it->second;
     allFollows.emplace(lineNum, vect[0]);
@@ -148,12 +165,12 @@ std::unordered_map<int, int> FollowTable::getAllFollows() {
 }
 
 std::unordered_map<int, std::vector<int>> FollowTable::getAllFollowsStar() {
-  return m_followTable;
+  return m_followMap;
 }
 
 std::vector<int> FollowTable::getFollowedByAnything() {
   std::vector<int> keys;
-  for (auto it = m_followTable.begin(); it != m_followTable.end(); ++it) {
+  for (auto it = m_followMap.begin(); it != m_followMap.end(); ++it) {
     int lineNum = it->first;
     keys.push_back(lineNum);
   }
@@ -168,7 +185,7 @@ std::vector<int> FollowTable::getFollowsAnything() {
 }
 
 bool FollowTable::hasFollowRelationship() {
-  if (m_followTable.size() > 0) {
+  if (m_followMap.size() > 0) {
     return true;
   } else {
     return false;
@@ -180,7 +197,7 @@ bool FollowTable::isFollowsAnything(int t_s2) {
 }
 
 bool FollowTable::isFollowedByAnything(int t_s1) {
-  for (auto it = m_followTable.begin(); it != m_followTable.end(); ++it) {
+  for (auto it = m_followMap.begin(); it != m_followMap.end(); ++it) {
     if (t_s1 == it->first) {
       return true;
     }
@@ -193,6 +210,7 @@ bool FollowTable::isFollowedByAnything(int t_s1) {
 * Instantiates an unordered map (hashmap) of line numbers to vector of line numbers associated.
 */
 FollowTable::FollowTable() {
-  std::unordered_map<int, std::vector<int>> m_followTable;
+  std::unordered_map<int, std::vector<int>> m_followMap;
+  std::unordered_map<int, std::vector<int>> m_followedByMap;
   std::set<int> m_allFollows;
 }
