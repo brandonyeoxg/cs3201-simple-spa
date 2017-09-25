70ff178d381bfc8de097cd4c98e807b9497b0144
peng229 <e0003632@u.nus.edu>
2017-09-25 18:21:23 +0800

    include more typedefs

--- source/GlobalTypeDef.h
+++ source/GlobalTypeDef.h
@@ -1,11 +1,14 @@
 #pragma once
 using namespace std;
+#include <vector>
 
 typedef int VAR_INDEX;
 typedef int PROC_INDEX;
 typedef int STMT_NUM;
 typedef std::string VAR_NAME;
 typedef int CONSTANT;
+typedef std::vector<int> LIST_OF_STMT_NUMS;
+typedef std::vector<std::string> LIST_OF_VAR_NAMES;
 
 const int INVALID_INDEX = -1;
 
