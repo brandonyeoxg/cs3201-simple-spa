c137f88660f64a8caa41c559c4d2afc20a34e118
DESKTOP-M3U9JB8\aaron <a0123977@u.nus.edu>
2017-09-25 15:10:07 +0800

    refactored QueryPreProcessor, added typedef for GlobalTypeDef.h

--- source/GlobalTypeDef.h
+++ source/GlobalTypeDef.h
@@ -7,6 +7,19 @@ typedef int STMT_NUM;
 typedef std::string VAR_NAME;
 typedef int CONSTANT;
 
+const int m_procedure = 0;
+const int m_statementList = 1;
+const int m_statement = 2;
+const int m_assign = 3;
+const int m_while = 4;
+const int m_if = 5;
+const int m_call = 6;
+const int m_variable = 7;
+const int m_constant = 8;
+const int m_progline = 9;
+const int m_statementNumber = 10;
+const int m_string = 11;
+
 const int INVALID_INDEX = -1;
 
 /*Called in parser.h, by brandon*/
