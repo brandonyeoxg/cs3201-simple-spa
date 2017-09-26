e33eab083f08c007fbdd65a484c0b96ce8874429
turtle96 <angjazlyn@hotmail.com>
2017-09-26 22:39:00 +0800

    add more unit tests for pattern match

--- UnitTesting/TestPatternMatch.cpp
+++ UnitTesting/TestPatternMatch.cpp
@@ -7,22 +7,65 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace UnitTesting {
   TEST_CLASS(TestPatternMatch) {
 public:
-  TEST_METHOD(getSubtreeStringsWithTokens) {
-    std::vector<std::string> tokens = { "x", "+", "y" };
+  TEST_METHOD(getSubtreeStringsWithTokens_patternLogic) {
+    std::vector<std::string> tokens, subtrees, expected;
 
-    //TODO add asserts for all these
+    // Test simple expression with only plus operator
+    tokens = { "x", "+", "y" };
+    expected = { "x+y", "x", "y" };
+    subtrees = PatternMatch::getInstance().getSubtreeStringsWithStmtTokens(tokens);
+    Assert::IsTrue(subtrees == expected);
 
+    // Longer expression with only plus operator
     tokens = { "x", "+", "y", "+", "z" };
+    expected = { "x+y+z", "x+y", "x", "y", "z" };
+    subtrees = PatternMatch::getInstance().getSubtreeStringsWithStmtTokens(tokens);
+    Assert::IsTrue(subtrees == expected);
 
+    // Long expression with plus and minus
     tokens = { "x", "+", "y", "+", "z", "-", "a" };
+    expected = { "x+y+z-a", "x+y+z", "x+y", "x", "y", "z", "a" };
+    subtrees = PatternMatch::getInstance().getSubtreeStringsWithStmtTokens(tokens);
+    Assert::IsTrue(subtrees == expected);
 
-    tokens = {"x", "+", "y", "*", "5", "+", "10"};
-
+    // Test plus and multiply
     tokens = { "x", "+", "y", "*", "z" };
+    expected = { "x+y*z", "x", "y*z", "y", "z" };
+    subtrees = PatternMatch::getInstance().getSubtreeStringsWithStmtTokens(tokens);
+    Assert::IsTrue(subtrees == expected);
 
-    std::vector<std::string> subtrees = PatternMatch::getInstance().getSubtreeStringsWithStmtTokens(tokens);
+    // Test plus and multiply, with constants
+    tokens = {"x", "+", "y", "*", "5", "+", "10"};
+    expected = { "x+y*5+10", "x+y*5", "x", "y*5", "y", "5", "10" };
+    subtrees = PatternMatch::getInstance().getSubtreeStringsWithStmtTokens(tokens);
+    Assert::IsTrue(subtrees == expected);
 
-    printVectorOfStrings(subtrees);
+  }
+
+  TEST_METHOD(getSubtreeStringsWithTokens_testWithDuplicates) {
+    std::vector<std::string> tokens, subtrees, expected;
+
+    // Test with trailing duplicates, each variable should not appear more than once
+    tokens = { "x", "+", "x", "+", "x" };
+    expected = { "x+x+x", "x+x", "x" };
+    subtrees = PatternMatch::getInstance().getSubtreeStringsWithStmtTokens(tokens);
+    Assert::IsTrue(subtrees == expected);
+  }
+
+  TEST_METHOD(getSubtreeStringsWithTokens_testWithMultiCharVariables) {
+    std::vector<std::string> tokens, subtrees, expected;
+
+    // Test with variables longer than 1 char
+    tokens = { "chicken", "+", "peanut", "-", "duck" };
+    expected = { "chicken+peanut-duck", "chicken+peanut", "chicken", "peanut", "duck" };
+    subtrees = PatternMatch::getInstance().getSubtreeStringsWithStmtTokens(tokens);
+    Assert::IsTrue(subtrees == expected);
+
+    // Ensure variables with same name but different case are included
+    tokens = { "chicken", "+", "CHICKEN", "-", "Chicken" };
+    expected = { "chicken+CHICKEN-Chicken", "chicken+CHICKEN", "chicken", "CHICKEN", "Chicken" };
+    subtrees = PatternMatch::getInstance().getSubtreeStringsWithStmtTokens(tokens);
+    Assert::IsTrue(subtrees == expected);
   }
 
 private:
