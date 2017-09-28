1b1eed0fc6e8905615d3e9aef005036db2fe79c3
brandonyeoxg <brandonyeoxg@gmail.com>
2017-09-28 13:28:12 +0800

    Update autotester automate.

--- AutoTester/autotester_automate.bat
+++ AutoTester/autotester_automate.bat
@@ -1,5 +1,5 @@
-start "" "%~dp0/../Debug/AutoTester.exe" %~dp0/simple-prog.txt %~dp0/simple-queries.txt %~dp0out.xml
-start "" "%~dp0/../Debug/AutoTester.exe" %~dp0/program/first.txt %~dp0/query/test-1.txt %~dp0out1.xml
-start "" "%~dp0/../Debug/AutoTester.exe" %~dp0/program/second.txt %~dp0/query/test-2.txt %~dp0out2.xml
-start "" "%~dp0/../Debug/AutoTester.exe" %~dp0/program/third.txt %~dp0/query/test-3.txt %~dp0out3.xml
-start "" "%~dp0/../Debug/AutoTester.exe" %~dp0/program/fourth.txt %~dp0/query/test-4.txt %~dp0out4.xml
+start "" "%~dp0../Debug/AutoTester.exe" %~dp0simple-prog.txt %~dp0simple-queries.txt %~dp0out.xml
+start "" "%~dp0../Debug/AutoTester.exe" %~dp0/program/first.txt %~dp0/query/test-1.txt %~dp0out1.xml
+start "" "%~dp0../Debug/AutoTester.exe" %~dp0/program/second.txt %~dp0/query/test-2.txt %~dp0out2.xml
+start "" "%~dp0../Debug/AutoTester.exe" %~dp0/program/third.txt %~dp0/query/test-3.txt %~dp0out3.xml
+start "" "%~dp0../Debug/AutoTester.exe" %~dp0/program/fourth.txt %~dp0/query/test-4.txt %~dp0out4.xml
