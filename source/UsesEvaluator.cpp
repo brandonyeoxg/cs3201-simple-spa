d83f76c42ecb6b0f424db1e6b5c619e411213628
Verbena <verbena_94@hotmail.com>
2017-10-01 04:24:22 +0800

    Refactor Uses Relationship.

--- source/UsesEvaluator.cpp
+++ source/UsesEvaluator.cpp
@@ -4,69 +4,40 @@
 
 bool UsesEvaluator::isRelationTrue(PkbReadOnly *t_pkb, Grammar t_g1, Grammar t_g2) {
   if (t_g2.getName() == "_") {
-    if (t_pkb->isFollowedByAnything(std::stoi(t_g1.getName()))) {
-      //std::cout << "Followed By Anything!\n";
+    if (t_pkb->isUsesAnything(std::stoi(t_g1.getName()))) {
+      //std::cout << "Is Uses Anything!\n";
       return true;
     } else {
-      //std::cout << "Does not Follow By Anything!\n";
-      return false;
-    }
-  } else if (t_g1.getName() == "_") {
-    if (t_pkb->isFollowsAnything(std::stoi(t_g2.getName()))) {
-      //std::cout << "Follows Anything!\n";
-      return true;
-    } else {
-      //std::cout << "Does not Follow Anything!\n";
+      //std::cout << "Does not Uses Anything!\n";
       return false;
     }
   } else {
-    if (t_pkb->isFollows(std::stoi(t_g1.getName()), std::stoi(t_g2.getName()))) {
-      //std::cout << "Follows: True\n";
+    if (t_pkb->isUses(std::stoi(t_g1.getName()), t_g2.getName())) {
+      //std::cout << "Uses: True\n";
       return true;
     } else {
-      //std::cout << "Follows: False\n";
+      //std::cout << "Uses: False\n";
       return false;
     }
   }
 }
 
 bool UsesEvaluator::hasRelationship(PkbReadOnly *t_pkb, Grammar t_g1, Grammar t_g2) {
-  if (t_pkb->hasFollowRelationship()) {
-    //std::cout << "Has Follows Relationship!\n";
-    return true;
-  } else {
-    //std::cout << "No Follows Relationship\n";
-    return false;
-  }
+  return false;
 }
 
 SET_OF_RESULTS UsesEvaluator::evaluateRightSynonym(PkbReadOnly *t_pkb, Grammar t_g1, Grammar t_g2) {
   std::unordered_map<int, Grammar::GType> typeOfStmts = t_pkb->getTypeOfStatementTable();
 
   if (t_g1.getType() == Grammar::GType::STMT_NO) {
-    int stmtNo;
-    try {
-      stmtNo = t_pkb->getFollows(std::stoi(t_g1.getName()));
-      //std::cout << "getFollows - STMT NO: " << stmtNo << "\n";
-    } catch (const std::invalid_argument& ia) {
-      //std::cout << "Invalid Argument Exception - No Results for getFollows(s1)\n";
+    std::vector<std::string> varUsedByStmt = t_pkb->getUses(std::stoi(t_g1.getName()));
+    if (varUsedByStmt.empty()) {
       return result;
     }
 
-    std::vector<std::string> stmtVector = filterStmts(typeOfStmts, stmtNo, t_g2);
-    result[t_g2.getName()] = stmtVector;
-  } else if (t_g1.getName() == "_") {
-    std::vector<int> stmtIntVector = t_pkb->getFollowsAnything();
-    if (stmtIntVector.empty()) {
-      return result;
-    }
-
-    std::vector<std::string> stmtStrVector;
-    for (auto& x : stmtIntVector) {
-      stmtStrVector = filterStmts(typeOfStmts, x, t_g2);
-    }
-
-    result[t_g2.getName()] = stmtStrVector;
+    result[t_g2.getName()] = varUsedByStmt;
+  } else if (t_g1.getType() == Grammar::GType::STR) {
+    
   }
 
   return result;
@@ -75,30 +46,26 @@ SET_OF_RESULTS UsesEvaluator::evaluateRightSynonym(PkbReadOnly *t_pkb, Grammar t
 SET_OF_RESULTS UsesEvaluator::evaluateLeftSynonym(PkbReadOnly *t_pkb, Grammar t_g1, Grammar t_g2) {
   std::unordered_map<int, Grammar::GType> typeOfStmts = t_pkb->getTypeOfStatementTable();
 
-  if (t_g2.getType() == Grammar::GType::STMT_NO) {
-    int stmtNo;
-    try {
-      stmtNo = t_pkb->getFollowedBy(std::stoi(t_g2.getName()));
-      //std::cout << "getFollowedBy - STMT NO: " << stmtNo << "\n";
-    } catch (const std::invalid_argument& ia) {
-      //std::cout << "Invalid Argument Exception - No Results for getFollowedBy(s2)\n";
+  if (t_g2.getType() == Grammar::GType::STR) {
+    std::vector<int> stmtIntVector = t_pkb->getStmtUses(t_g2.getName());
+    if (stmtIntVector.empty()) {
       return result;
     }
-
-    std::vector<std::string> stmtVector = filterStmts(typeOfStmts, stmtNo, t_g1);
-    result[t_g1.getName()] = stmtVector;
+    
+    std::vector<std::string> stmtStrVector = filterStmts(typeOfStmts, stmtIntVector, t_g1);
+    if (!stmtStrVector.empty()) {
+      result[t_g1.getName()] = stmtStrVector;
+    }
   } else if (t_g2.getName() == "_") {
-    std::vector<int> stmtIntVector = t_pkb->getFollowedByAnything();
+    std::vector<int> stmtIntVector = t_pkb->getStmtUsesAnything();
     if (stmtIntVector.empty()) {
       return result;
     }
 
-    std::vector<std::string> stmtStrVector;
-    for (auto& x : stmtIntVector) {
-      stmtStrVector = filterStmts(typeOfStmts, x, t_g1);
+    std::vector<std::string> stmtStrVector = filterStmts(typeOfStmts, stmtIntVector, t_g1);
+    if (!stmtStrVector.empty()) {
+      result[t_g1.getName()] = stmtStrVector;
     }
-
-    result[t_g1.getName()] = stmtStrVector;
   }
 
   return result;
@@ -107,14 +74,18 @@ SET_OF_RESULTS UsesEvaluator::evaluateLeftSynonym(PkbReadOnly *t_pkb, Grammar t_
 SET_OF_RESULTS UsesEvaluator::evaluateBothSynonyms(PkbReadOnly *t_pkb, Grammar t_g1, Grammar t_g2) {
   std::unordered_map<int, Grammar::GType> typeOfStmts = t_pkb->getTypeOfStatementTable();
 
-  std::unordered_map<int, int> allFollows = t_pkb->getAllFollows();
-  if (allFollows.empty()) {
+  std::unordered_map<std::string, std::vector<int>> stmtsAndVar = t_pkb->getAllStmtUses();
+  if (stmtsAndVar.empty()) {
     return result;
   }
 
-  for (auto& x : allFollows) {
-    std::vector<std::string> stmtVector = filterStmts(typeOfStmts, x.second, t_g2);
-    result[std::to_string(x.first)] = filterStmts(typeOfStmts, x.first, t_g1, stmtVector);
+  for (auto& x : stmtsAndVar) {
+    if (!x.second.empty()) {
+      std::vector<std::string> stmtVector = filterStmts(typeOfStmts, x.second, t_g1);
+      if (!stmtVector.empty()) {
+        result[x.first] = stmtVector;
+      }
+    } 
   }
 
   return result;
