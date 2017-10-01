c9040866e9592e9055bbf9a137eec72b4369bb4c
brandonyeoxg <brandonyeoxg@gmail.com>
2017-10-01 20:39:58 +0800

    Add ModifiesP implementation.

---
+++ source/ModifiesP.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <unordered_set>
+#include "GlobalTypeDef.h"
+
+/**
+* Represents the modifies relationship for procedures.
+*
+* @author Brandon
+* @date 29/9/2017
+*/
+class ModifiesP
+{
+public:
+  ModifiesP() {};
+  ~ModifiesP() {};
+
+  void insertModifiesP(PROC_INDEX t_procIdx, PROC_NAME t_procName, VAR_INDEX t_varIdx, VAR_NAME t_varName);
+  bool isModifiesP(PROC_INDEX t_procIdx, VAR_INDEX t_varIdx); /*< Modifies("First", "x") */
+  bool isModifiesInProc(PROC_INDEX t_idx); /*< Modifies("First", _) */
+  LIST_OF_VAR_NAMES getVarNamesWithProcIdx(PROC_INDEX t_idx); /*< Modifies("First", x) */
+  LIST_OF_PROC_NAMES getProcNamesWithVarIdx(VAR_INDEX t_idx); /*< Modifies(p, "x") */
+  MAP_OF_PROC_TO_VAR& getAllProcToVar(); /*< Modifies(p, x) */
+  LIST_OF_PROC_NAMES& getAllProcNames(); /*< Modifies(p, _) */
+
+private:
+  std::unordered_map<PROC_INDEX, LIST_OF_VAR_NAMES> m_procToVarNames;
+  std::unordered_map<PROC_INDEX, VAR_HASH_SET> m_procToVarSet;
+  std::unordered_map<VAR_INDEX, LIST_OF_PROC_NAMES> m_varToProcNames;
+  MAP_OF_PROC_TO_VAR m_allProcNamesToVarNames;
+  LIST_OF_PROC_NAMES m_procNames;
+
+  void insertModifiesWithProcAsKey(PROC_INDEX t_procIdx, VAR_NAME t_varName);
+  void insertModifiesWithProcAsKeyWithVarHash(PROC_INDEX t_procIdx, VAR_INDEX t_varIdx);
+  void insertModifiesWithVarAsKey(VAR_INDEX t_varIdx, PROC_NAME t_procName);
+  void insertModifiesProcName(PROC_NAME& t_name);
+  void insertModifiesProcAndVarName(PROC_NAME t_procName, VAR_NAME t_varName);
+};
+
