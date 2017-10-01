e4b72757d925c2e62d94ab7c839e05d70e31be8a
peng229 <e0003632@u.nus.edu>
2017-10-01 17:18:04 +0800

    Implement insertUsesForStmt method

---
+++ source/UsesTable.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <set>
+
+#include "GlobalTypeDef.h"
+
+
+class UsesTable {
+public:
+  void insertUsesForStmt(VAR_NAME t_varName, STMT_NUM t_lineNum);
+  void insertToUsesStmtMap(STMT_NUM t_lineNum, VAR_NAME t_varName);
+  bool isUses(STMT_NUM t_lineNum, VAR_NAME t_varName);
+  LIST_OF_VAR_NAMES getUses(STMT_NUM t_line_num);
+  LIST_OF_STMT_NUMS getStmtUses(VAR_NAME t_varName);
+  std::unordered_map<VAR_NAME, LIST_OF_STMT_NUMS> getAllStmtUses();
+  bool isUsesAnything(STMT_NUM t_line_num);  //uses(2, _)
+  LIST_OF_STMT_NUMS getStmtUsesAnything(); //uses(s, _)
+
+  UsesTable();
+  std::unordered_map<STMT_NUM, LIST_OF_VAR_NAMES> getUsesStmtMap();
+  std::unordered_map<VAR_NAME, LIST_OF_STMT_NUMS> getUsesVarMap();
+
+private:
+  std::unordered_map<STMT_NUM, LIST_OF_VAR_NAMES> m_usesStmtMap;
+  std::unordered_map<VAR_NAME, LIST_OF_STMT_NUMS> m_usesVarMap;
+};
