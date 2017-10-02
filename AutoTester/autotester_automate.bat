6ea74824b27e6bd1bf4753c3800cf72b148980a9
Verbena <verbena_94@hotmail.com>
2017-10-02 14:59:49 +0800

    Add the validation tests in the automate batch file.

--- AutoTester/autotester_automate.bat
+++ AutoTester/autotester_automate.bat
@@ -3,3 +3,4 @@ start "" "%~dp0../Debug/AutoTester.exe" %~dp0/program/first.txt %~dp0/query/test
 start "" "%~dp0../Debug/AutoTester.exe" %~dp0/program/second.txt %~dp0/query/test-2.txt %~dp0out2.xml
 start "" "%~dp0../Debug/AutoTester.exe" %~dp0/program/third.txt %~dp0/query/test-3.txt %~dp0out3.xml
 start "" "%~dp0../Debug/AutoTester.exe" %~dp0/program/fourth.txt %~dp0/query/test-4.txt %~dp0out4.xml
+start "" "%~dp0../Debug/AutoTester.exe" %~dp0/program/validation-program.txt %~dp0/query/validation-testcases.txt %~dp0out5.xml
