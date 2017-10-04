55d568f86d28d00a11b8b3589012205c7ed24bfa
Verbena <verbena_94@hotmail.com>
2017-10-04 16:40:45 +0800

    Add functions in the Relation class to check the RType of the Relation objects.

--- source/Relation.cpp
+++ source/Relation.cpp
@@ -49,3 +49,51 @@ Relation::Relation(std::string t_type, Grammar t_g1, Grammar t_g2) {
   m_g1 = t_g1;
   m_g2 = t_g2;
 }
+
+bool Relation::isFollows(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::FOLLOWS;
+}
+
+bool Relation::isFollowsStar(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::FOLLOWS_;
+}
+
+bool Relation::isParent(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::PARENT;
+}
+
+bool Relation::isParentStar(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::PARENT_;
+}
+
+bool Relation::isUses(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::USES;
+}
+
+bool Relation::isModifies(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::MODIFIES;
+}
+
+bool Relation::isCalls(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::CALLS;
+}
+
+bool Relation::isCallsStar(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::CALLS_;
+}
+
+bool Relation::isNext(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::NEXT;
+}
+
+bool Relation::isNextStar(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::NEXT_;
+}
+
+bool Relation::isAffects(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::AFFECTS;
+}
+
+bool Relation::isAffectsStar(Relation t_relation) {
+  return t_relation.getType() == queryType::RType::AFFECTS_;
+}
