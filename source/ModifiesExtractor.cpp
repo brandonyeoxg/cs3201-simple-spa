19359c05e54a7f64a400f4251e9bad56fba336a3
peng229 <e0003632@u.nus.edu>
2017-10-04 16:05:25 +0800

    implement extractors for uses/modifies by call stmt

--- source/ModifiesExtractor.cpp
+++ source/ModifiesExtractor.cpp
@@ -8,5 +8,29 @@ void ModifiesExtractor::populateModifiesByCallStatements() {
   CallsTable* callsTable = m_pkb->getCallsTable();
   StatementTable* statementTable = m_pkb->getStatementTable();
   ParentTable* parentTable = m_pkb->getParentTable();
-  ModifiesP* ModifiesP = m_pkb->getModifiesP();
+  ModifiesP* modifiesP = m_pkb->getModifiesP();
+  ModifiesTable* modifiesTable = m_pkb->getModifiesTable();
+  ProcTable* procTable = m_pkb->getProcTable();
+
+  //get all call statements
+  LIST_OF_STMT_NUMS allCallStmts = statementTable->getListOfStatements(queryType::GType::CALL);
+
+  //for every call statement
+  for (int i = 0; i <= allCallStmts.size(); i++) {
+    //1. Get proc name from call Stmt no.
+    PROC_NAME procName = callsTable->getProcNameFromCallStmtNum(allCallStmts[i]);
+    //2. Get proc index from proc name.
+    PROC_INDEX procIdx = procTable->getProcIdxFromName(procName);
+    //3. Get list of var names involved for modifies in the procedure.
+    LIST_OF_VAR_NAMES listOfModifiesVarNames = modifiesP->getVarNamesWithProcIdx(procIdx);
+    LIST_OF_STMT_NUMS allParentStars = parentTable->getParentStarOf(allCallStmts[i]);
+
+    for (int j = 0; j <= allParentStars.size(); j++) {
+      //for every parent* of the statement
+      //set all these varNames for parent*
+      for (int k = 0; k <= listOfModifiesVarNames.size(); k++) {
+        modifiesTable->insertModifiesForStmt(listOfModifiesVarNames[k], allParentStars[j]);
+      }
+    }
+  }
 }
--- source/UsesExtractor.cpp
+++ source/UsesExtractor.cpp
@@ -9,4 +9,28 @@ void UsesExtractor::populateUsesByCallStatements() {
   StatementTable* statementTable = m_pkb->getStatementTable();
   ParentTable* parentTable = m_pkb->getParentTable();
   UsesP* usesP = m_pkb->getUsesP();
+  UsesTable* usesTable = m_pkb->getUsesTable();
+  ProcTable* procTable = m_pkb->getProcTable();
+  
+  //get all call statements
+  LIST_OF_STMT_NUMS allCallStmts = statementTable->getListOfStatements(queryType::GType::CALL);
+
+  //for every call statement
+  for (int i = 0; i <= allCallStmts.size(); i++) {
+    //1. Get proc name from call Stmt no.
+    PROC_NAME procName = callsTable->getProcNameFromCallStmtNum(allCallStmts[i]);
+    //2. Get proc index from proc name.
+    PROC_INDEX procIdx = procTable->getProcIdxFromName(procName);
+    //3. Get list of var names involved for uses in the procedure.
+    LIST_OF_VAR_NAMES listOfUsesVarNames = usesP->getVarNamesWithProcIdx(procIdx);
+    LIST_OF_STMT_NUMS allParentStars = parentTable->getParentStarOf(allCallStmts[i]);
+
+    for (int j = 0; j <= allParentStars.size(); j++) {
+      //for every parent* of the statement
+      //set all these varNames for parent*
+      for (int k = 0; k <= listOfUsesVarNames.size(); k++) {
+        usesTable->insertUsesForStmt(listOfUsesVarNames[k], allParentStars[j]);
+      }
+    }
+  }
 }
