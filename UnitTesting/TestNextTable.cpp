7be51c135ef016e2cfd1549b73de4a5847199486
turtle96 <angjazlyn@hotmail.com>
2017-10-09 23:43:00 +0800

    some testing

---
+++ UnitTesting/TestNextTable.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "NextTable.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting {
+  TEST_CLASS(TestNextTable) {
+public:
+
+  TEST_METHOD(isNext) {
+    NextTable nextTable = NextTable();
+    nextTable.insertNext(0, 1);
+    nextTable.insertNext(0, 2);
+    nextTable.insertNext(0, 3);
+
+    Assert::IsTrue(nextTable.isNext(0, 3));
+    Assert::IsTrue(nextTable.isNext(0, 1));
+    Assert::IsFalse(nextTable.isNext(1, 3));
+  }
+
+  TEST_METHOD(isNextStar) {
+    NextTable nextTable = NextTable();
+    nextTable.insertNext(0, 1);
+    nextTable.insertNext(0, 2);
+    nextTable.insertNext(0, 3);
+    nextTable.insertNext(2, 1);
+
+    Assert::IsFalse(nextTable.isNext(1, 3));
+    Assert::IsTrue(nextTable.isNextStar(1, 3));
+
+    Assert::IsFalse(nextTable.isNextStar(0, 5));
+    nextTable.insertNext(2, 4);
+    nextTable.insertNext(4, 5);
+    Assert::IsTrue(nextTable.isNextStar(0, 5));
+  }
+
+  };
+}
