91b783cc51db555321198310aa5561bb4c838a5f
Verbena <verbena_94@hotmail.com>
2017-10-10 12:58:14 +0800

    Add parameters to the functions in IntermediateTable.h.

--- source/IntermediateTable.h
+++ source/IntermediateTable.h
@@ -6,21 +6,21 @@
 
 class IntermediateTable {
 public:
-  void insertOneSynonym(SYNONYM_NAME t_name, LIST_OF_RESULTS);
-  void insertTwoSynonym(SYNONYM_NAME, SYNONYM_NAME, SET_OF_RESULTS);
-  LIST_OF_RESULTS getResults(LIST_OF_SYNONYMS);
+  void insertOneSynonym(SYNONYM_NAME t_synonym, LIST_OF_RESULTS t_results);
+  void insertTwoSynonym(SYNONYM_NAME t_synonym1, SYNONYM_NAME t_synonym2, SET_OF_RESULTS t_results);
+  LIST_OF_RESULTS getResults(LIST_OF_SYNONYMS t_synonymList);
 
 protected:
   INTERMEDIATE_TABLE m_results;
   std::unordered_map<SYNONYM_NAME, SYNONYM_POSITION> m_synonymRowChecker;
 
-  bool hasSynonym(SYNONYM_NAME);
+  bool hasSynonym(SYNONYM_NAME t_synonym);
 
 private:
   std::vector<bool> m_ignoreColumn;
 
-  INTERMEDIATE_TABLE getCartesianProduct(LIST_OF_RESULTS);
-  INTERMEDIATE_TABLE getCartesianProduct(SET_OF_RESULTS);
-  LIST_OF_RESULTS getCommonResults(LIST_OF_RESULTS);
-  void removeNonCommonResults(SET_OF_RESULTS);
+  INTERMEDIATE_TABLE getCartesianProduct(LIST_OF_RESULTS t_results);
+  INTERMEDIATE_TABLE getCartesianProduct(SET_OF_RESULTS t_results);
+  LIST_OF_RESULTS getCommonResults(LIST_OF_RESULTS t_results);
+  void removeNonCommonResults(SET_OF_RESULTS t_results);
 };
