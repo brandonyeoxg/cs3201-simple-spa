ab78d7c3471138e4b9712bd77f35939000401691
Verbena <verbena_94@hotmail.com>
2017-10-10 13:02:02 +0800

    Implement insertOneSynonym and insertTwoSynonym functions in IntermediateTable.cpp.

--- source/IntermediateTable.cpp
+++ source/IntermediateTable.cpp
@@ -1,5 +1,45 @@
 #include "IntermediateTable.h"
 
+void IntermediateTable::insertOneSynonym(SYNONYM_NAME t_synonym, LIST_OF_RESULTS t_results) {
+  if (!hasSynonym(t_synonym)) {
+    //insertSynonym(t_synonym);
+    m_results = getCartesianProduct(t_results);
+  } else {
+    m_results = getCartesianProduct(getCommonResults(t_results));
+  }
+}
+
+void IntermediateTable::insertTwoSynonym(SYNONYM_NAME t_synonym1, SYNONYM_NAME t_synonym2, SET_OF_RESULTS t_results) {
+  if (!hasSynonym(t_synonym1) && !hasSynonym(t_synonym2)) {
+    //insertSynonym(t_synonym1);
+    //insertSynonym(t_synonym2);
+    m_results = getCartesianProduct(t_results);
+  } else if (hasSynonym(t_synonym1) && !hasSynonym(t_synonym2)) {
+    //insertSynonym(t_synonym2);
+    std::vector<std::string> resultsOfFirstSynonym;// = getResultsBySynonym(t_synonym1, t_results);
+    m_results = getCartesianProduct((getCommonResults(resultsOfFirstSynonym)));
+  } else if (!hasSynonym(t_synonym1) && hasSynonym(t_synonym2)) {
+    //insertSynonym(t_synonym1);
+    std::vector<std::string> resultsOfSecondSynonym;// = getResultsBySynonym(t_synonym2, t_results);
+    m_results = getCartesianProduct((getCommonResults(resultsOfSecondSynonym)));
+  } else if (hasSynonym(t_synonym1) && hasSynonym(t_synonym2)) {
+    removeNonCommonResults(t_results);
+    m_results = getCartesianProduct(t_results);
+  }
+}
+
+INTERMEDIATE_TABLE IntermediateTable::getCartesianProduct(LIST_OF_RESULTS t_results) {
+
+}
+
+INTERMEDIATE_TABLE IntermediateTable::getCartesianProduct(SET_OF_RESULTS t_results) {
+
+}
+
+LIST_OF_RESULTS IntermediateTable::getCommonResults(LIST_OF_RESULTS t_results) {
+
+}
+
 bool IntermediateTable::hasSynonym(SYNONYM_NAME t_name) {
   auto rowItr = m_synonymRowChecker.find(t_name);
   return rowItr != m_synonymRowChecker.end();
@@ -31,4 +71,3 @@ LIST_OF_RESULTS IntermediateTable::getResults(LIST_OF_SYNONYMS t_selectedSyn) {
   }
   return output;
 }
-
