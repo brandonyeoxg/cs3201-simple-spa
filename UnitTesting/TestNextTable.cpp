41070a0ede321437fc6d9c8fd38678596a2ead34
turtle96 <angjazlyn@hotmail.com>
2017-10-11 01:28:25 +0800

    rewrite unit tests

--- UnitTesting/TestNextTable.cpp
+++ UnitTesting/TestNextTable.cpp
@@ -10,28 +10,39 @@ public:
 
   TEST_METHOD(isNext) {
     NextTable nextTable = NextTable();
-    nextTable.insertNext(0, 1);
-    nextTable.insertNext(0, 2);
-    nextTable.insertNext(0, 3);
+    nextTable.insertNextRelationship(0, 1);
+    nextTable.insertNextRelationship(0, 2);
+    nextTable.insertNextRelationship(0, 3);
+    nextTable.insertNextRelationship(0, 500);
+
+    nextTable.executeAfterAllNextInserts();
+
+    Assert::IsFalse(nextTable.isNext(1, 3));
 
     Assert::IsTrue(nextTable.isNext(0, 3));
     Assert::IsTrue(nextTable.isNext(0, 1));
-    Assert::IsFalse(nextTable.isNext(1, 3));
+    Assert::IsTrue(nextTable.isNext(0, 500));
   }
 
   TEST_METHOD(isNextStar) {
     NextTable nextTable = NextTable();
-    nextTable.insertNext(0, 1);
-    nextTable.insertNext(0, 2);
-    nextTable.insertNext(0, 3);
-    nextTable.insertNext(2, 1);
+    nextTable.insertNextRelationship(0, 1);
+    nextTable.insertNextRelationship(0, 2);
+    nextTable.insertNextRelationship(0, 3);
+    nextTable.insertNextRelationship(2, 1);
+
+    nextTable.executeAfterAllNextInserts();
+
+    Assert::IsTrue(nextTable.isNextStar(0, 1));
+    Assert::IsTrue(nextTable.isNextStar(0, 1));
 
     Assert::IsFalse(nextTable.isNext(1, 3));
-    Assert::IsTrue(nextTable.isNextStar(1, 3));
+    Assert::IsFalse(nextTable.isNextStar(1, 3));
 
     Assert::IsFalse(nextTable.isNextStar(0, 5));
-    nextTable.insertNext(2, 4);
-    nextTable.insertNext(4, 5);
+    nextTable.insertNextRelationship(2, 5);
+    //nextTable.insertNextRelationship(4, 5);
+    nextTable.executeAfterAllNextInserts();
     Assert::IsTrue(nextTable.isNextStar(0, 5));
   }
 
