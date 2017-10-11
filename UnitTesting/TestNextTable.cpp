7ed648232a084a59899101d20db9898fbb9b3c58
turtle96 <angjazlyn@hotmail.com>
2017-10-11 14:40:11 +0800

    more tests

--- UnitTesting/TestNextTable.cpp
+++ UnitTesting/TestNextTable.cpp
@@ -50,6 +50,44 @@ public:
 
   }
 
+  TEST_METHOD(isNext_isNextStar_01) {
+    NextTable nextTable = NextTable();
+
+    // basically an if statement, with a while statement nested in else section
+    // if-statement: line 1, while-statement: line 3
+    nextTable.insertNextRelationship(1, 2);
+    nextTable.insertNextRelationship(1, 3);
+    nextTable.insertNextRelationship(3, 4);
+    nextTable.insertNextRelationship(4, 3);
+    nextTable.insertNextRelationship(3, 5);
+    nextTable.insertNextRelationship(2, 5);
+
+    nextTable.executeAfterAllNextInserts();
+
+    // test all possible control paths for Next()
+    Assert::IsTrue(nextTable.isNext(1, 2));
+    Assert::IsTrue(nextTable.isNext(1, 3));
+    Assert::IsTrue(nextTable.isNext(3, 4));
+    Assert::IsTrue(nextTable.isNext(4, 3));
+    Assert::IsTrue(nextTable.isNext(3, 5));
+    Assert::IsTrue(nextTable.isNext(2, 5));
+
+    // test for false paths
+    Assert::IsFalse(nextTable.isNext(1, 5));
+    Assert::IsFalse(nextTable.isNext(2, 3));
+    Assert::IsFalse(nextTable.isNext(2, 4));
+    Assert::IsFalse(nextTable.isNext(2, 1));
+    Assert::IsFalse(nextTable.isNext(4, 5));
+    Assert::IsFalse(nextTable.isNext(3, 3));
+
+    // test for possible control paths for Next*()
+    Assert::IsTrue(nextTable.isNextStar(1, 2));
+    Assert::IsTrue(nextTable.isNextStar(1, 5));
+    Assert::IsTrue(nextTable.isNextStar(1, 4));
+    Assert::IsTrue(nextTable.isNextStar(4, 5));
+    Assert::IsTrue(nextTable.isNextStar(3, 3));
+  }
+
 private:
 
   void printGraph(std::unordered_map<PROG_LINE, std::vector<PROG_LINE>> graph) {
