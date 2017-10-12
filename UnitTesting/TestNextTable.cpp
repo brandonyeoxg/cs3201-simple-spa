978b346ebc1b5ae9357db701446f19d4f32925c2
turtle96 <angjazlyn@hotmail.com>
2017-10-13 01:52:20 +0800

    add test

--- UnitTesting/TestNextTable.cpp
+++ UnitTesting/TestNextTable.cpp
@@ -308,6 +308,8 @@ public:
     nextTable.insertNextRelationship(3, 2);
     nextTable.insertNextRelationship(2, 1);
 
+    nextTable.executeAfterAllNextInserts();
+
     std::unordered_map<PROG_LINE, std::vector<PROG_LINE>> map = nextTable.getAllNextStar();
     expected = { 1, 2, 3, 4 };
 
@@ -321,6 +323,56 @@ public:
       Assert::IsTrue(iter.second == expected);  // Check lines reachable for each line
     }
 
+  }
+
+  TEST_METHOD(getAllLinesAfterAnyLine_01) {
+    NextTable nextTable = NextTable();
+    std::vector<PROG_LINE> expected, result;
+
+    /*  1   if ...
+        2     stmt
+            else
+        3     while ...
+        4       stmt
+        5       stmt
+        6       stmt
+    */
+    nextTable.insertNextRelationship(1, 2);
+    nextTable.insertNextRelationship(1, 3);
+    nextTable.insertNextRelationship(3, 4);
+    nextTable.insertNextRelationship(4, 5);
+    nextTable.insertNextRelationship(5, 6);
+    nextTable.insertNextRelationship(6, 3);
+
+    nextTable.executeAfterAllNextInserts();
+
+    expected = { 2, 3, 4, 5, 6 };
+    result = nextTable.getAllLinesAfterAnyLine();
+    Assert::IsTrue(expected == result);
+  }
+
+  TEST_METHOD(getAllLinesAfterAnyLine_02) {
+    NextTable nextTable = NextTable();
+    std::vector<PROG_LINE> expected, result;
+
+    /*  1   while ...
+        2     while ...
+        3       while ...
+        4         stmt
+    */
+
+    nextTable.insertNextRelationship(1, 2);
+    nextTable.insertNextRelationship(2, 3);
+    nextTable.insertNextRelationship(3, 4);
+    nextTable.insertNextRelationship(4, 3);
+    nextTable.insertNextRelationship(3, 2);
+    nextTable.insertNextRelationship(2, 1);
+
+    nextTable.executeAfterAllNextInserts();
+
+    expected = { 1, 2, 3, 4 };
+    result = nextTable.getAllLinesAfterAnyLine();
+    Assert::IsTrue(expected == result);
   }
 
 private:
