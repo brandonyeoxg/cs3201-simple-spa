a45af63e2fb067d2686fd27ac02877e1fd1ed182
Verbena <verbena_94@hotmail.com>
2017-10-13 15:23:26 +0800

    Edit the error in the With Class.

--- source/With.cpp
+++ source/With.cpp
@@ -2,10 +2,11 @@
 
 With::With() {}
 
-Grammar Relation::getG1() { return m_g1; }
-Grammar Relation::getG2() { return m_g2; }
-
 With::With(Grammar t_g1, Grammar t_g2) {
   m_g1 = t_g1;
   m_g2 = t_g2;
 }
+
+Grammar With::getG1() { return m_g1; }
+Grammar With::getG2() { return m_g2; }
+
