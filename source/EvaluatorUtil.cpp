75d995613ad20026198e942b8330a9ccf1ea43a0
Verbena <verbena_94@hotmail.com>
2017-10-13 16:48:41 +0800

    Add a QueryUtil and EvaluatorUtil class.

---
+++ source/EvaluatorUtil.cpp
@@ -0,0 +1,2 @@
+#include "EvaluatorUtil.h"
+
---
+++ source/EvaluatorUtil.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+#include <unordered_map>
+
+#include "GlobalTypeDef.h"
+#include "Grammar.h"
+
+class EvaluatorUtil {
+public:
+  
+};
