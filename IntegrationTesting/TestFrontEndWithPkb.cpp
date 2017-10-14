b95a8ad2f76c21d9bf0d8583583d2bcaad543119
brandonyeoxg <brandonyeoxg@gmail.com>
2017-10-14 17:09:56 +0800

    Add more test cases for integration testing.

--- IntegrationTesting/TestFrontEndWithPkb.cpp
+++ IntegrationTesting/TestFrontEndWithPkb.cpp
@@ -22,11 +22,11 @@ namespace IntegrationTesting
       m_parser = new ParserDriver(m_pkb);
       std::ofstream tmpFile;
       m_tmpFileName = "testSimple.txt";
-      m_tmpFile.open(m_tmpFileName);
     }
 
     TEST_METHOD_CLEANUP(CleanupPkbAndParser) 
     {
+      m_tmpFile.close();
       std::remove(m_tmpFileName.c_str());
       delete m_pkb;
       delete m_parser;
@@ -35,9 +35,7 @@ namespace IntegrationTesting
 		TEST_METHOD(TestParserAndPKBProcedure)
 		{
       // Test for the procedure recorded properly
-      m_tmpFile << "procedure main {\n";
-      m_tmpFile.close();
-      m_parser->openFileStream(m_tmpFileName);
+      editSimpleProgramFile("procedure main {\n");
       m_parser->parseProcedure();
 
       ProcTable* procTable = m_pkb->getProcTable();
@@ -49,10 +47,9 @@ namespace IntegrationTesting
 
     TEST_METHOD(TestParserAndPKBFollows) // This only tests follows relation
     {
-      m_tmpFile << "procedure main {x=y;}\n";
-      m_tmpFile.close();
-      m_parser->openFileStream(m_tmpFileName);
       LIST_OF_STMT_NUMS dummyStmtList;
+      editSimpleProgramFile("procedure main {\n x=y;}\n");
+      m_parser->parseProcedure();
       m_parser->parseStmt(dummyStmtList);
 
       // Follow table should be empty
@@ -62,10 +59,7 @@ namespace IntegrationTesting
       delete m_parser;
 
       m_parser = new ParserDriver(m_pkb);
-      m_tmpFile.open(m_tmpFileName);
-      m_tmpFile << "x=y;\ny=x;\n";
-      m_tmpFile.close();
-      m_parser->openFileStream(m_tmpFileName);
+      editSimpleProgramFile("x=y;\ny=x;\n");
       m_parser->parseStmt(dummyStmtList);
       dummyStmtList.push_back(1);
       m_parser->parseStmt(dummyStmtList);
@@ -79,9 +73,7 @@ namespace IntegrationTesting
 
     TEST_METHOD(TestParserAndPKBParent)
     {
-      m_tmpFile << "procedure main {\n x=y;}\n";
-      m_tmpFile.close();
-      m_parser->openFileStream(m_tmpFileName);
+      editSimpleProgramFile("procedure main {\n x=y;}\n");
       LIST_OF_STMT_NUMS dummyStmtList;
       m_parser->parseProcedure();
       m_parser->parseStmt(dummyStmtList);
@@ -90,14 +82,31 @@ namespace IntegrationTesting
       LIST_OF_STMT_NUMS actual = parentTable->getParentOfAnything();
       Assert::AreEqual(actual.size(), size_t(0));
 
-      m_tmpFile.open(m_tmpFileName);
-      m_tmpFile << "while i { \nx=y;}";
-      m_tmpFile.close();
-      m_parser->openFileStream(m_tmpFileName);
+      editSimpleProgramFile("while i { \nx=y;}");
       m_parser->parseStmt(dummyStmtList);
-      
       actual = parentTable->getParentOfAnything();
       Assert::AreEqual(actual.size(), size_t(1));
+
+      editSimpleProgramFile("while i {\n while j { \n x=y;}}");
+      m_parser->parseStmt(dummyStmtList);
+      actual = parentTable->getParentOfAnything();
+      Assert::AreEqual(actual.size(), size_t(3));
+      actual = parentTable->getChildrenOfAnything();
+      Assert::AreEqual(actual.size(), size_t(3));
+    }
+
+    TEST_METHOD(TestParserAndPKBModifiesP)
+    {
+      editSimpleProgramFile("procedure main {\n x=y;}");
+      m_parser->parseProcedure();
+      
+    }
+
+    void editSimpleProgramFile(std::string t_editText) {
+      m_tmpFile.open(m_tmpFileName);
+      m_tmpFile << t_editText;
+      m_tmpFile.close();
+      m_parser->openFileStream(m_tmpFileName);
     }
 	};
 }
