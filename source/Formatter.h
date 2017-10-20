06d3326260ef2c449125b57672524bef1f828f38
Verbena <verbena_94@hotmail.com>
2017-10-20 16:34:13 +0800

    Add a public function to format string vector to map of string and string vector.

--- source/Formatter.h
+++ source/Formatter.h
@@ -38,5 +38,13 @@ public:
   * @return the contents of a map<int, str> in a map<str, vector<str>>.
   */
   static SET_OF_RESULTS formatMapIntStrToMapStrVectorStr(std::unordered_map<int, std::string> t_mapIntStr);
+
+  /**
+  * A public function to format string vector to map<str, vector<str>>.
+  * Given a string vector, it will format it into a map<str, vector<str>>
+  * by duplicating the string vector for the key and value.
+  * @return the contents of a string vector in a map<str, vector<str>>.
+  */
+  static SET_OF_RESULTS formatVectorStrToMapStrVectorStr(LIST_OF_RESULTS t_vectorStr);
 };
 
