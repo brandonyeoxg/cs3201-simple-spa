7f4673df7d0441502e32e2d3348bd3fec0fa784e
brandonyeoxg <brandonyeoxg@gmail.com>
2017-10-29 10:56:34 +0800

    Finish up required api calls on StatementTable to be called in the AffectsExTractor

--- source/StatementTable.h
+++ source/StatementTable.h
@@ -16,17 +16,22 @@ public:
   void insertStatementIntoStatementTable(STMT_NUM t_lineNum, queryType::GType t_type, PROC_INDEX t_procIdx, PROC_NAME t_procName);
   BOOLEAN insertTypeOfStatementTable(STMT_NUM t_lineNum, queryType::GType t_type);
   BOOLEAN insertStatementTypeTable(queryType::GType t_type, STMT_NUM t_lineNum);
+  void insertAsProcToStmt(STMT_NUM t_lineNum, PROC_INDEX t_procIdx);
+  void insertAsStmtToProc(STMT_NUM t_lineNum, PROC_NAME t_procName);
+
 
   MAP_OF_STMT_NUM_TO_GTYPE getTypeOfStatementTable();
   MAP_OF_GTYPE_TO_LIST_OF_STMT_NUMS  getStatementTypeTable();
   LIST_OF_STMT_NUMS getListOfStatements(queryType::GType t_type);
   TOTAL_NUMBER_OF_STMTS getNumberOfStatements();
 
+  LIST_OF_STMT_NUMS getStmtsFromProcIdx(PROC_INDEX t_procIdx);
+  PROC_NAME getProcNameFromStmtNum(STMT_NUM t_lineNum);
 
 private:
   std::unordered_map<STMT_NUM, queryType::GType> m_typeOfStatementTable;
   std::unordered_map<queryType::GType,LIST_OF_STMT_NUMS> m_statementTypeTable;
 
-  MAP_OF_PROC_NAME_TO_LIST_OF_STMT_NUMS m_procNameToStmts;
+  MAP_OF_PROC_INDEX_TO_LIST_OF_STMT_NUMS m_procIdxToStmts;
   MAP_OF_STMT_NUM_TO_PROC_NAME m_stmtToProcName;
 };
