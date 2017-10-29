38a3b3dac911debfad79f99a10724c9f65613c3e
brandonyeoxg <brandonyeoxg@gmail.com>
2017-10-29 15:24:40 +0800

    Add more functionality to the statement table. Add test for Affects.

--- source/AffectsTable.cpp
+++ source/AffectsTable.cpp
@@ -11,8 +11,21 @@ BOOLEAN AffectsTable::hasAffectsFromBounds(STMT_NUM t_startBound, STMT_NUM t_end
 
 PAIR_OF_AFFECTS_LIST AffectsTable::getAffectsListsFromBounds(STMT_NUM t_startBound, STMT_NUM t_endBound) {
   // Checks if the start point type
-  const std::map<PROG_LINE, std::vector<PROG_LINE>> *readOnlyCFG = m_pkbTablesOnly->getNextTable()->getAfterGraph();
-  auto cItnextLines  = readOnlyCFG->at(t_startBound);
-  std::vector<PROG_LINE> stmts = readOnlyCFG->at(t_startBound);
+  const std::map<PROG_LINE, std::vector<PROG_LINE>> *readOnlyCFG = m_nextTable->getAfterGraph();
+  // Check if the startBound is a container stmt
+  traverseCfg(t_startBound, t_endBound);
   return {};
+}
+
+void AffectsTable::traverseCfg(PROG_LINE m_curProgLine, PROG_LINE m_endBound) {
+  // Checks for the current line if it is a container stmt
+  queryType::GType stmtType = m_stmtTable->getTypeOfStatement(m_curProgLine);
+  if (isContainerStmt(stmtType)) {
+    // Get the 2 stmts that leads to the container stmts
+    LIST_OF_STMT_NUMS stmts = m_nextTable->getAfterGraph()->at(m_curProgLine);
+  }
+}
+
+BOOLEAN AffectsTable::isContainerStmt(queryType::GType t_type) {
+  return t_type == queryType::GType::IF || t_type == queryType::GType::WHILE;
 }
