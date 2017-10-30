903d7c9fda7dc9667c8048ef1247fa5e8b89d42b
turtle96 <angjazlyn@hotmail.com>
2017-10-30 21:02:48 +0800

    rest of get methods

--- source/pql/QueryCache.cpp
+++ source/pql/QueryCache.cpp
@@ -4,12 +4,17 @@
 
 QueryCache::QueryCache() {
   m_allNextStar = nullptr;
+
   m_followsAnything = nullptr;
   m_followedByAnything = nullptr;
+  
   m_childrenOfAnything = nullptr;
   m_parentOfAnything = nullptr;
   m_childrenStarOfAnything = nullptr;
   m_parentStarOfAnything = nullptr;
+
+  m_stmtUsesAnything = nullptr;
+  m_stmtModifiesAnything = nullptr;
 }
 
 QueryCache::~QueryCache() {
@@ -20,8 +25,45 @@ QueryCache::~QueryCache() {
   delete m_parentOfAnything;
   delete m_childrenStarOfAnything;
   delete m_parentStarOfAnything;
+  delete m_stmtModifiesAnything;
 }
 
 MAP_OF_PROG_LINE_TO_LIST_OF_PROG_LINES * QueryCache::getAllNextStar() {
   return m_allNextStar;
+}
+
+MAP_OF_STMT_NUMS * QueryCache::getAllFollows() {
+  return m_allFollows;
+}
+
+LIST_OF_STMT_NUMS * QueryCache::getFollowsAnything() {
+  return m_followsAnything;
+}
+
+LIST_OF_STMT_NUMS * QueryCache::getFollowedByAnything() {
+  return m_followedByAnything;
+}
+
+LIST_OF_STMT_NUMS * QueryCache::getChildrenOfAnything() {
+  return m_childrenOfAnything;
+}
+
+LIST_OF_STMT_NUMS * QueryCache::getParentOfAnything() {
+  return m_parentOfAnything;
+}
+
+LIST_OF_STMT_NUMS * QueryCache::getChildrenStarOfAnything() {
+  return m_childrenStarOfAnything;
+}
+
+LIST_OF_STMT_NUMS * QueryCache::getParentStarOfAnything() {
+  return m_parentStarOfAnything;
+}
+
+LIST_OF_STMT_NUMS * QueryCache::getStmtUsesAnything() {
+  return m_stmtUsesAnything;
+}
+
+LIST_OF_STMT_NUMS * QueryCache::getStmtModifiesAnything() {
+  return m_stmtModifiesAnything;
 }
--- source/pql/QueryCache.h
+++ source/pql/QueryCache.h
@@ -3,7 +3,8 @@
 #include "../GlobalTypeDef.h"
 
 /** Class for caching results intra-query.
-*   Currently caches results for Relationships involving 2 common synonyms.
+*   Currently caches results for Relationships involving 2 common synonyms,
+*   or 1 common synonym and 1 wild card.
 *   @author jazlyn
 */
 
@@ -14,11 +15,23 @@ public:
 
   MAP_OF_PROG_LINE_TO_LIST_OF_PROG_LINES * getAllNextStar();
 
-  MAP_OF_STMT_NUMS  *getAllFollows();
+  MAP_OF_STMT_NUMS  * getAllFollows();
 
   LIST_OF_STMT_NUMS * getFollowsAnything();
 
   LIST_OF_STMT_NUMS * getFollowedByAnything();
+
+  LIST_OF_STMT_NUMS * getChildrenOfAnything();
+
+  LIST_OF_STMT_NUMS * getParentOfAnything();
+
+  LIST_OF_STMT_NUMS * getChildrenStarOfAnything();
+
+  LIST_OF_STMT_NUMS * getParentStarOfAnything();
+
+  LIST_OF_STMT_NUMS * getStmtUsesAnything();
+
+  LIST_OF_STMT_NUMS * getStmtModifiesAnything();
 
 private:
 
@@ -33,6 +46,11 @@ private:
   LIST_OF_STMT_NUMS *m_childrenStarOfAnything;           /**< Parent*(_, s1) */
   LIST_OF_STMT_NUMS *m_parentStarOfAnything;             /**< Parent*(s1, _) */
 
+  LIST_OF_STMT_NUMS *m_stmtUsesAnything;                 /**< Uses(s1, _) */
+  LIST_OF_STMT_NUMS *m_stmtModifiesAnything;             /**< Modifies(s1, _) */
+
   //m_allWhileStmtsWithVar; /**< Pattern w(v, _) */
   //  m_allIfStmtsWithVar;  /**<  Pattern ifs(v, _) */
+
+  // affects
 };
