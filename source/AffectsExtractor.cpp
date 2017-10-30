af3504dc01492198fa44b7c8fff987ed6a7dca45
brandonyeoxg <brandonyeoxg@gmail.com>
2017-10-30 16:22:31 +0800

    Start scaffolding for Affects*

--- source/AffectsExtractor.cpp
+++ source/AffectsExtractor.cpp
@@ -38,3 +38,39 @@ BOOLEAN AffectsExtractor::extractIsAffectsAnything(STMT_NUM t_modifiesLine) { //
 BOOLEAN AffectsExtractor::extractIsAffectedByAnything(STMT_NUM t_usesLines) { // affects(_,12)
   return false;
 }
+
+SET_OF_AFFECTS AffectsExtractor::extractAllAffectsStar() { // affects*(a1,a2)
+  return{};
+}
+
+LIST_OF_AFFECTS_STMTS AffectsExtractor::extractAffectsStar(STMT_NUM t_modifiesLine) { // affects*(2,a)
+  return{};
+}
+
+LIST_OF_AFFECTS_STMTS AffectsExtractor::extractAffectedByStar(STMT_NUM t_usesLine) { // affects*(a,12)
+  return{};
+}
+
+BOOLEAN AffectsExtractor::extractIsAffectsStar(STMT_NUM t_modifiesLine, STMT_NUM t_usesLine) { // affects*(1,12)
+  return false;
+}
+
+BOOLEAN AffectsExtractor::extractHasAffectsRelationshipStar() { // affects*(_,_)
+  return false;
+}
+
+LIST_OF_AFFECTS_STMTS AffectsExtractor::extractAffectsAnythingStar() {  // affects*(a,_)
+  return{};
+}
+
+LIST_OF_AFFECTS_STMTS AffectsExtractor::extractAffectedByAnythingStar() { // affects*(_,a)
+  return{};
+}
+
+BOOLEAN AffectsExtractor::extractIsAffectsAnythingStar(STMT_NUM t_modifiesLine) { // affects*(1,_)
+  return false;
+}
+
+BOOLEAN AffectsExtractor::extractIsAffectedByAnythingStar(STMT_NUM t_usesLines) { // affects*(_,12)
+  return false;
+}
