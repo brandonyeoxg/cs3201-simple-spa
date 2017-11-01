f82ca26649bc5ba96a40ad190a66f903b6f41611
Verbena <verbena_94@hotmail.com>
2017-11-01 16:49:40 +0800

    Change the paramater of getResult function in IntermediateResult class from string vector to grammar vector.

--- source/IntermediateTable.cpp
+++ source/IntermediateTable.cpp
@@ -45,7 +45,7 @@ bool IntermediateTable::insertTwoSynonym(SYNONYM_NAME t_synonym1, SYNONYM_NAME t
   return true;
 }
 
-LIST_OF_RESULTS IntermediateTable::getResults(LIST_OF_SYNONYMS t_selectedSyn) {
+LIST_OF_RESULTS IntermediateTable::getResults(std::vector<Grammar> t_selectedSyn) {
   LIST_OF_RESULTS output;
   if (t_selectedSyn.size() == 0) {
     return {};
@@ -54,12 +54,16 @@ LIST_OF_RESULTS IntermediateTable::getResults(LIST_OF_SYNONYMS t_selectedSyn) {
   for (int i = 0; i < depthOfTable; i++) {
     STRING_TOKEN result = "";
     for (auto& sItr : t_selectedSyn) {
-      auto& synInTable = m_synonymRowChecker.find(sItr);
-      if (synInTable == m_synonymRowChecker.end()) {
-        return {};
+      if (!Grammar::isStmtNo(sItr.getType()) && !Grammar::isString(sItr.getType())) {
+        auto& synInTable = m_synonymRowChecker.find(sItr.getName());
+        if (synInTable == m_synonymRowChecker.end()) {
+          return{};
+        }
+        int synPosInTable = synInTable->second;
+        result += m_results[i][synPosInTable] + " ";
+      } else {
+        result += sItr.getName() + " ";
       }
-      int synPosInTable = synInTable->second;
-      result += m_results[i][synPosInTable] + " ";
     }
     result.pop_back();
     output.push_back(result);
--- source/IntermediateTable.h
+++ source/IntermediateTable.h
@@ -6,6 +6,8 @@
 #include <unordered_map>
 
 #include "GlobalTypeDef.h"
+#include "Grammar.h"
+#include "PkbReadOnly.h"
 
 class IntermediateTable {
 public:
@@ -17,7 +19,7 @@ public:
   * Returns an empty list If no synonyms selected or synonyms selected does not exist in the intermediate table.
   * @param t_synonyms list of synonyms to determine which results is output in the intermediate table.
   */
-  LIST_OF_RESULTS getResults(LIST_OF_SYNONYMS t_synonyms);
+  LIST_OF_RESULTS getResults(std::vector<Grammar> t_synonyms);
   BOOLEAN hasSynonyms();
 
   /*
