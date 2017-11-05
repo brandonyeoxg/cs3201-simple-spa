4bf384b92ee4f2e21907676d71224e41e7ecdabc
Verbena <verbena_94@hotmail.com>
2017-11-05 16:59:04 +0800

    Implement the getClauseType function in Relation, Pattern and With class.

--- source/With.cpp
+++ source/With.cpp
@@ -7,6 +7,8 @@ With::With(Grammar t_g1, Grammar t_g2) {
   m_g2 = t_g2;
 }
 
+queryType::clauseType With::getClauseType() { return queryType::clauseType::WITH; }
+
 Grammar With::getG1() { return m_g1; }
 Grammar With::getG2() { return m_g2; }
 
