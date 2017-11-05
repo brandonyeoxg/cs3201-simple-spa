dd66667457c03be19d0eeb53ad4db2a0c8fbe05b
turtle96 <angjazlyn@hotmail.com>
2017-11-05 17:42:54 +0800

    add methods to clause

--- source/Clause.h
+++ source/Clause.h
@@ -5,4 +5,14 @@
 class Clause {
 public:
   virtual queryType::clauseType getClauseType();
+
+  /** Checks if given Clause is of type Pattern.
+  *   @author jazlyn
+  */
+  bool isPatternType();
+
+  /** Checks if given Clause is of type Relation.
+  *   @author jazlyn
+  */
+  bool isRelationType();
 };
