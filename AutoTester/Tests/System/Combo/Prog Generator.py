b9295f3d07bed75d41a86f7052d3ab040ac0f783
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2017-11-07 01:12:52 +0800

    Created an even longer program file to test all clauses at once, hopefully up to 500 queries.
    Created a code generator so I don't have to type as many lines, also creates a lot of variables. Likely very stressful for SPA.

---
+++ AutoTester/Tests/System/Combo/Prog Generator.py
@@ -0,0 +1,21 @@
+def gen(num):
+    for i in range(num):
+        print("  stress"+str(i)+" = test"+str(i+1)+";")
+    return None
+
+def gen2(num):
+    for i in range(-num,num):
+        print("  test"+str(abs(i-round(num/2)))+" = stress"+str(i-1)+";")
+    return None
+
+def gen3(num):
+    for i in range(-num*2, round(num/2)):
+        print("  thankYou"+str(abs(i))+" = Python"+str(i+3*num)+";")
+    return None
+
+def gen4(num):
+    for i in range(num%7, round(num*3-1)):
+        print("  master"+str(abs(i))+" = Hacka"+str(i+3*num)+";")
+    return None
+
+gen4(87)
