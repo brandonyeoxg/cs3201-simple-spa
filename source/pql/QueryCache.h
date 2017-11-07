df416a39401b5427c9eeb01cb81332ab2610f918
turtle96 <angjazlyn@hotmail.com>
2017-11-08 00:14:21 +0800

    document

--- source/pql/QueryCache.h
+++ source/pql/QueryCache.h
@@ -9,8 +9,10 @@
 #include "../QueryUtil.h"
 
 /** Class for caching results intra-query.
-*   Currently caches results for Relationships involving 2 common synonyms,
-*   or 1 common synonym and 1 wildcard. 
+*   Currently caches results for: 
+*     Relationships involving 2 common synonyms
+*     Relationships involving 1 common synonym and 1 wildcard
+*     Patterns involving 1 common synonym
 *   Caches results that PKB takes more than O(1) time to deliver.
 *   @author jazlyn
 */
