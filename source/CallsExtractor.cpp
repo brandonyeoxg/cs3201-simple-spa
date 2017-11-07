e35ae51c0e5ad5536de89b3b67ef0dcce047eaa1
peng229 <e0003632@u.nus.edu>
2017-11-07 17:12:05 +0800

    implement hasLoopingCalls method in callsExtractor that checks "looping calls" in source code.

--- source/CallsExtractor.cpp
+++ source/CallsExtractor.cpp
@@ -6,6 +6,25 @@ void CallsExtractor::extractDesign() {
   //Need to populate the calls* relationships first, so that the indices version can be populated in the below method.
   populateCallsIdx();
 }
+
+/*A mechanism to check if there exists a situation where procedure A calls B,
+* and B calls A (or transitive closure of such situation). 
+*/
+void CallsExtractor::hasLoopingCalls() {
+  CallsTable* callsTable = m_pkb->getCallsTable();
+  MAP_OF_PROC_INDEX_TO_LIST_OF_PROC_INDICES callsStarMap = callsTable->getCalledByStarMapByIdx();
+  //for every key in calls* map,
+  for (auto it = callsStarMap.begin(); it != callsStarMap.end(); ++it) {
+    //if the list contains the key, throw exception.
+    PROC_INDEX key = it->first;
+    LIST_OF_PROC_INDICES list = it->second;
+    if (std::find(list.begin(), list.end(), key) != list.end()) {
+      //throws exception here! @Brandon
+    }
+  }
+
+}
+
 void CallsExtractor::populateCallsIdx() {
   CallsTable* callsTable = m_pkb->getCallsTable();
   ProcTable* procTable = m_pkb->getProcTable();
