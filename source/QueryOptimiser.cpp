eeee532c8e8516d1a3d4a794a64718f5dd2a9f7a
Verbena <verbena_94@hotmail.com>
2017-11-07 18:39:50 +0800

    Implement divideClausesIntoGroups function in QueryOptimiser.

--- source/QueryOptimiser.cpp
+++ source/QueryOptimiser.cpp
@@ -3,111 +3,177 @@
 #include "QueryOptimiser.h"
 
 void QueryOptimiser::divideClausesIntoGroups(std::priority_queue<Clause*> &t_noSyns, std::priority_queue<std::priority_queue<Clause*>*> &t_withSyns) {
-  int relationSize = m_relations.size();
-  int patternSize = m_patterns.size();
-  int withSize = m_withs.size();
-  std::priority_queue<Clause*> syns;
-  std::priority_queue<std::priority_queue<Clause*>*> allGroupsWithSyns;
+  std::queue<Clause*> syns;
+  std::vector<std::vector<Clause*>*> allGroupsWithSyns;
 
-  for (int i = 0; i < relationSize; ++i) {
-    Relation *relPtr = &m_relations.front();
-    syns.push(relPtr);
+  for (auto& relation : m_relations) {
+    Relation *relPtr = &relation;
     if (QueryUtil::hasNoSynonyms(relPtr->getG1(), relPtr->getG2())) {
       t_noSyns.push(relPtr);
     } else { 
       syns.push(relPtr);
     }
-
-    m_relations.pop();
   }
 
-  for (int i = 0; i < patternSize; ++i) {
-    Pattern *patPtr = &m_patterns.front();
-    if (QueryUtil::hasNoSynonyms(patPtr->getLeft(), patPtr->getRight())) {
-      t_noSyns.push(patPtr);
-    } else {
-      syns.push(patPtr);
-    }
-
-    m_patterns.pop();
+  for (auto& pattern : m_patterns) {
+    Pattern *patPtr = &pattern;
+    syns.push(patPtr);
   }
 
-  for (int i = 0; i < withSize; ++i) {
-    With *withPtr = &m_withs.front();
+  for (auto& with : m_withs) {
+    With *withPtr = &with;
     if (QueryUtil::hasNoSynonyms(withPtr->getG1(), withPtr->getG2())) {
       t_noSyns.push(withPtr);
     } else {
       syns.push(withPtr);
     }
-
-    m_withs.pop();
   }
+  
+  std::vector<Clause*> *newGroup = new std::vector<Clause*>();
+  newGroup->push_back(syns.front());
+  allGroupsWithSyns.push_back(newGroup);
+  syns.pop();
 
-  int synsSize = syns.size();
-  for (int i = 0; i < synsSize; ++i) {
-    Clause *clause = syns.top();
-    int numOfGroups = allGroupsWithSyns.size();
-    
-    for (int grp = 0; grp < numOfGroups; ++grp) {
-      std::priority_queue<Clause*> *existingGroup = allGroupsWithSyns.top();
-      int numOfClauses = existingGroup->size();
-      bool hasCommon = false;
-      for (int cls = 0; cls < numOfClauses; ++cls) {
-        Clause *existingClause = existingGroup->top();
-        if (existingClause->getClauseType() == queryType::clauseType::RELATION) {
+  int numOfGroups = allGroupsWithSyns.size();
+  for (int i = 0; i < numOfGroups; ++i) {
+    //std::vector<Clause*> existingGroup = *allGroupsWithSyns[i];
+    int numOfClauses = (*allGroupsWithSyns[i]).size();
+    for (int j = 0; j < numOfClauses; ++j) {
+      Clause *existingClause = (*allGroupsWithSyns[i])[j];
+      int numOfSyns = syns.size();
+      for (int k = 0; k < numOfSyns; ++k) {
+        bool hasCommon = false;
+        Clause *clause = syns.front();
+        if (existingClause->getClauseType() == queryType::clauseType::RELATION 
+          && clause->getClauseType() == queryType::clauseType::RELATION) {
           Relation *existingRelation = (Relation*)existingClause;
           Relation *relation = (Relation*)clause;
           if (relation->getG1().getName() == existingRelation->getG1().getName()
             || relation->getG1().getName() == existingRelation->getG2().getName()
             || relation->getG2().getName() == existingRelation->getG1().getName()
             || relation->getG2().getName() == existingRelation->getG2().getName()) {
-            existingGroup->push(clause);
             hasCommon = true;
-            break;
           }
-        } else if (existingClause->getClauseType() == queryType::clauseType::PATTERN) {
+        } else if (existingClause->getClauseType() == queryType::clauseType::RELATION
+          && clause->getClauseType() == queryType::clauseType::PATTERN) {
+          Relation *existingRelation = (Relation*)existingClause;
+          Pattern *pattern = (Pattern*)clause;
+          if (pattern->getStmt().getName() == existingRelation->getG1().getName()
+            || pattern->getStmt().getName() == existingRelation->getG2().getName()
+            || pattern->getLeft().getName() == existingRelation->getG1().getName()
+            || pattern->getLeft().getName() == existingRelation->getG2().getName()) {
+            hasCommon = true;
+          }
+        } else if (existingClause->getClauseType() == queryType::clauseType::RELATION
+          && clause->getClauseType() == queryType::clauseType::WITH) {
+          Relation *existingRelation = (Relation*)existingClause;
+          With *with = (With*)clause;
+          if (with->getG1().getName() == existingRelation->getG1().getName()
+            || with->getG1().getName() == existingRelation->getG2().getName()
+            || with->getG2().getName() == existingRelation->getG1().getName()
+            || with->getG2().getName() == existingRelation->getG2().getName()) {
+            hasCommon = true;
+          }
+        } else if (existingClause->getClauseType() == queryType::clauseType::PATTERN
+          && clause->getClauseType() == queryType::clauseType::PATTERN) {
           Pattern *existingPattern = (Pattern*)existingClause;
           Pattern *pattern = (Pattern*)clause;
-          if (pattern->getLeft().getName() == existingPattern->getLeft().getName()
-            || pattern->getLeft().getName() == existingPattern->getRight().getName()
-            || pattern->getRight().getName() == existingPattern->getLeft().getName()
-            || pattern->getRight().getName() == existingPattern->getRight().getName()) {
-            existingGroup->push(clause);
+          if (pattern->getStmt().getName() == existingPattern->getStmt().getName()
+            || pattern->getStmt().getName() == existingPattern->getLeft().getName()
+            || pattern->getLeft().getName() == existingPattern->getStmt().getName()
+            || pattern->getLeft().getName() == existingPattern->getLeft().getName()) {
             hasCommon = true;
-            break;
           }
-        } else if (existingClause->getClauseType() == queryType::clauseType::WITH) {
+        } else if (existingClause->getClauseType() == queryType::clauseType::PATTERN
+          && clause->getClauseType() == queryType::clauseType::RELATION) {
+          Pattern *existingPattern = (Pattern*)existingClause;
+          Relation *relation = (Relation*)clause;
+          if (relation->getG1().getName() == existingPattern->getStmt().getName()
+            || relation->getG1().getName() == existingPattern->getLeft().getName()
+            || relation->getG2().getName() == existingPattern->getStmt().getName()
+            || relation->getG2().getName() == existingPattern->getLeft().getName()) {
+            hasCommon = true;
+          }
+        } else if (existingClause->getClauseType() == queryType::clauseType::PATTERN
+          && clause->getClauseType() == queryType::clauseType::WITH) {
+          Pattern *existingPattern = (Pattern*)existingClause;
+          With *with = (With*)clause;
+          if (with->getG1().getName() == existingPattern->getStmt().getName()
+            || with->getG1().getName() == existingPattern->getLeft().getName()
+            || with->getG2().getName() == existingPattern->getStmt().getName()
+            || with->getG2().getName() == existingPattern->getLeft().getName()) {
+            hasCommon = true;
+          }
+        } else if (existingClause->getClauseType() == queryType::clauseType::WITH
+          && clause->getClauseType() == queryType::clauseType::WITH) {
           With *existingWith = (With*)existingClause;
           With *with = (With*)clause;
           if (with->getG1().getName() == existingWith->getG1().getName()
             || with->getG1().getName() == existingWith->getG2().getName()
             || with->getG2().getName() == existingWith->getG1().getName()
             || with->getG2().getName() == existingWith->getG2().getName()) {
-            existingGroup->push(clause);
             hasCommon = true;
-            break;
+          }
+        } else if (existingClause->getClauseType() == queryType::clauseType::WITH
+          && clause->getClauseType() == queryType::clauseType::RELATION) {
+          With *existingWith = (With*)existingClause;
+          Relation *relation = (Relation*)clause;
+          if (relation->getG1().getName() == existingWith->getG1().getName()
+            || relation->getG1().getName() == existingWith->getG2().getName()
+            || relation->getG2().getName() == existingWith->getG1().getName()
+            || relation->getG2().getName() == existingWith->getG2().getName()) {
+            hasCommon = true;
+          }
+        } else if (existingClause->getClauseType() == queryType::clauseType::WITH
+          && clause->getClauseType() == queryType::clauseType::PATTERN) {
+          With *existingWith = (With*)existingClause;
+          Pattern *pattern = (Pattern*)clause;
+          if (pattern->getStmt().getName() == existingWith->getG1().getName()
+            || pattern->getStmt().getName() == existingWith->getG2().getName()
+            || pattern->getLeft().getName() == existingWith->getG1().getName()
+            || pattern->getLeft().getName() == existingWith->getG2().getName()) {        
+            hasCommon = true;
           }
         }
-      }
+    
+        if (hasCommon) {
+          (*allGroupsWithSyns[i]).push_back(clause);
+          numOfClauses++;
+        } else {
+          syns.push(clause);
+        }
 
-      allGroupsWithSyns.pop();
-      allGroupsWithSyns.push(existingGroup);
-
-      if (hasCommon) {
-        break;
+        syns.pop(); 
       }
     }
 
-    std::priority_queue<Clause*> *newGroup = new std::priority_queue<Clause*>();
-    newGroup->push(clause);
-    allGroupsWithSyns.push(newGroup);
-    syns.pop();
+    if (!syns.empty()) {
+      std::vector<Clause*> *newGroup = new std::vector<Clause*>();
+      newGroup->push_back(syns.front());
+      allGroupsWithSyns.push_back(newGroup);
+      numOfGroups++;
+      syns.pop();
+    }  
   }
 
-  std::cout << "HELLO";
-  /*int numOfGroups = allGroupsWithSyns.size();
+  for (auto& group : allGroupsWithSyns) {
+    std::priority_queue<Clause*> *grp = new std::priority_queue<Clause*>();
+    for (auto& clause : *group) {
+      grp->push(clause);
+    }
+
+    t_withSyns.push(grp);
+  }
+
+  for (auto* group : allGroupsWithSyns) {
+    delete group;
+  }
+}
+
+void QueryOptimiser::sortBetweenGroups(std::priority_queue<std::priority_queue<Clause*>*> &t_groups) {
+  /*int numOfGroups = t_groups.size();
   for (int grp = 0; grp < numOfGroups; ++grp) {
-    std::priority_queue<Clause*> *existingGroup = allGroupsWithSyns.front();
+    std::priority_queue<Clause*> *existingGroup = t_groups.top;
     int numOfClauses = existingGroup->size();
     bool isSelected = false;
     for (int cls = 0; cls < numOfClauses; ++cls) {
@@ -138,13 +204,6 @@ void QueryOptimiser::divideClausesIntoGroups(std::priority_queue<Clause*> &t_noS
       t_noSelectSyns.push(existingGroup);
     }
   }*/
-}
-
-void QueryOptimiser::sortBetweenGroups(std::priority_queue<std::priority_queue<Clause*>> &t_groups) {
-  int size = t_groups.size();
-  for (int grp = 0; grp < size; ++grp) {
-    //std::priority_queue<Clause*> group = t_groups.top;
-  }
 }
 
 void QueryOptimiser::sortWithinGroups(std::priority_queue<Clause*> &t_clauses) {
--- source/QueryOptimiser.h
+++ source/QueryOptimiser.h
@@ -10,19 +10,19 @@
 
 class QueryOptimiser {
 public:
-  QueryOptimiser(std::queue<Grammar> t_selects, std::queue<Relation> t_relations, std::queue<Pattern> t_patterns, std::queue<With> t_withs)
+  QueryOptimiser(std::vector<Grammar> t_selects, std::vector<Relation> t_relations, std::vector<Pattern> t_patterns, std::vector<With> t_withs)
     : m_selects(t_selects),
       m_relations(t_relations),
       m_patterns(t_patterns),
       m_withs(t_withs) {};
 
   void divideClausesIntoGroups(std::priority_queue<Clause*> &t_noSyns, std::priority_queue<std::priority_queue<Clause*>*> &t_withSyns);
-  void sortBetweenGroups(std::priority_queue<std::priority_queue<Clause*>> &t_groups);
+  void sortBetweenGroups(std::priority_queue<std::priority_queue<Clause*>*> &t_groups);
   void sortWithinGroups(std::priority_queue<Clause*> &t_clauses);
 
 private:
-  std::queue<Grammar> m_selects;
-  std::queue<Relation> m_relations;
-  std::queue<Pattern> m_patterns;
-  std::queue<With> m_withs;
+  std::vector<Grammar> m_selects;
+  std::vector<Relation> m_relations;
+  std::vector<Pattern> m_patterns;
+  std::vector<With> m_withs;
 };
