fb8e092de4a3aa7fe853cd8a8035a977687f3750
turtle96 <angjazlyn@hotmail.com>
2017-11-07 23:07:03 +0800

    comment

--- source/pql/QueryCache.h
+++ source/pql/QueryCache.h
@@ -55,21 +55,36 @@ class QueryCache {
 public:
   QueryCache();
 
+  /** Gets cached result with given clause.
+  *   Given clause must be cacheable (will be checked with assert), use isCacheable() to check.
+  *   @param t_clause clause, assumes it is a cacheable clause
+  *   @return pointer to set of results, will return nullptr if no results cached
+  */
   SET_OF_RESULTS *getCache(Clause *t_clause);
 
+  /** Caches given result with given clause.
+  *   Given clause must be cacheable (will be checked with assert), use isCacheable() to check.
+  *   @param t_clause clause, assumes it is a cacheable clause
+  *   @param t_results set of results
+  */
   void cache(Clause *t_clause, SET_OF_RESULTS t_results);
 
+  /** Checks if given clause is cacheable.
+  *   @param t_clause clause
+  *   @return true if clause is cacheable, else false
+  */
   bool isCacheable(Clause *t_clause);
 
+  ////////// exposed for debugging only
+  // Converts a Clause object of Pattern or Relation type to a string to be used as a key
   std::string getKey(Clause &t_clause);
 
 private:
-  std::unordered_map<std::string, SET_OF_RESULTS> m_cache;
+  std::unordered_map<std::string, SET_OF_RESULTS> m_cache;  /**< Cache that maps each Clause (in string form) to its set of results */
 
   bool isPatternCacheable(Pattern *t_pattern);
   bool isRelationCacheable(Relation *t_relation);
 
-  
   std::string getKeyWithPattern(Pattern t_pattern);
   std::string getKeyWithRelation(Relation t_relation);
   std::string getKeyWithGrammar(Grammar t_grammar);
