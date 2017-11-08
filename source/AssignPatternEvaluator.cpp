7e85e7c6d6bc7c375044512c3a3314eddfc9d2ae
Verbena <verbena_94@hotmail.com>
2017-11-09 03:41:35 +0800

    Refactor from string to integer in all the Relation and Pattern Evaluators.

--- source/AssignPatternEvaluator.cpp
+++ source/AssignPatternEvaluator.cpp
@@ -2,110 +2,110 @@
 
 #include "AssignPatternEvaluator.h"
 
-SET_OF_RESULTS AssignPatternEvaluator::getAllStmtsWithVarAndExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+SET_OF_RESULTS_INDICES AssignPatternEvaluator::getAllStmtsWithVarAndExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
   LIST_OF_STMT_NUMS assignStmts = t_pkb->getAllAssignStmtByVarAndExactPattern(t_g1.getName(), t_g2.getVector());
   if (assignStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(assignStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(assignStmts);
+  m_result[0] = assignStmts;
   return m_result;
 }
 
-SET_OF_RESULTS AssignPatternEvaluator::getAllStmtsWithVarAndSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+SET_OF_RESULTS_INDICES AssignPatternEvaluator::getAllStmtsWithVarAndSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
   LIST_OF_STMT_NUMS assignStmts = t_pkb->getAllAssignStmtByVarAndSubtreePattern(t_g1.getName(), t_g2.getVector());
   if (assignStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(assignStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(assignStmts);
+  m_result[0] = assignStmts;
   return m_result;
 }
 
-SET_OF_RESULTS AssignPatternEvaluator::getAllStmtsWithVarAndAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+SET_OF_RESULTS_INDICES AssignPatternEvaluator::getAllStmtsWithVarAndAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
   LIST_OF_STMT_NUMS assignStmts = t_pkb->getAllAssignStmtListByVar(t_g1.getName());
   if (assignStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(assignStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(assignStmts);
+  m_result[0] = assignStmts;
   return m_result;
 }
 
-SET_OF_RESULTS AssignPatternEvaluator::getAllStmtsWithExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+SET_OF_RESULTS_INDICES AssignPatternEvaluator::getAllStmtsWithExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
   LIST_OF_STMT_NUMS assignStmts = t_pkb->getAllAssignStmtByExactPattern(t_g2.getVector());
   if (assignStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(assignStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(assignStmts);
+  m_result[0] = assignStmts;
   return m_result;
 }
 
-SET_OF_RESULTS AssignPatternEvaluator::getAllStmtsWithSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+SET_OF_RESULTS_INDICES AssignPatternEvaluator::getAllStmtsWithSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
   LIST_OF_STMT_NUMS assignStmts = t_pkb->getAllAssignStmtBySubtreePattern(t_g2.getVector());
   if (assignStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(assignStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(assignStmts);
+  m_result[0] = assignStmts;
   return m_result;
 }
 
-SET_OF_RESULTS AssignPatternEvaluator::getAllStmtsWithAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::vector<int> allAssignStmts = t_pkb->getAllAssignStmts();
+SET_OF_RESULTS_INDICES AssignPatternEvaluator::getAllStmtsWithAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  LIST_OF_STMT_NUMS allAssignStmts = t_pkb->getAllAssignStmts();
   if (allAssignStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(allAssignStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(allAssignStmts);
+  m_result[0] = allAssignStmts;
   return m_result;
 }
 
-SET_OF_RESULTS AssignPatternEvaluator::getAllStmtsAndVarWithExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::unordered_map<STMT_NUM, VAR_INDEX> assignStmtsWithVar = t_pkb->getAllAssignStmtWithVarByExactPattern(t_g2.getVector());
+SET_OF_RESULTS_INDICES AssignPatternEvaluator::getAllStmtsAndVarWithExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  MAP_OF_STMT_NUM_TO_VAR_INDEX assignStmtsWithVar = t_pkb->getAllAssignStmtWithVarByExactPattern(t_g2.getVector());
   if (assignStmtsWithVar.empty()) {
     return m_result;
   }
 
-  std::unordered_map<STMT_NUM, VAR_NAME> assignStmtsWithVarName;
+  /*std::unordered_map<STMT_NUM, VAR_NAME> assignStmtsWithVarName;
   for (auto& x : assignStmtsWithVar) {
     VAR_NAME varName = t_pkb->getVarNameFromIdx(x.second);
     assignStmtsWithVarName[x.first] = varName;
-  }
+  }*/
 
-  m_result = Formatter::formatMapIntStrToMapStrVectorStr(assignStmtsWithVarName);
+  m_result = Formatter::formatMapIntIntToMapIntVectorInt(assignStmtsWithVar);
   return m_result;
 }
 
-SET_OF_RESULTS AssignPatternEvaluator::getAllStmtsAndVarWithSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::unordered_map<STMT_NUM, VAR_INDEX> assignStmtsWithVar = t_pkb->getAllAssignStmtWithVarBySubtreePattern(t_g2.getVector());
+SET_OF_RESULTS_INDICES AssignPatternEvaluator::getAllStmtsAndVarWithSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  MAP_OF_STMT_NUM_TO_VAR_INDEX assignStmtsWithVar = t_pkb->getAllAssignStmtWithVarBySubtreePattern(t_g2.getVector());
   if (assignStmtsWithVar.empty()) {
     return m_result;
   }
 
-  std::unordered_map<STMT_NUM, VAR_NAME> assignStmtsWithVarName;
+  /*std::unordered_map<STMT_NUM, VAR_NAME> assignStmtsWithVarName;
   for (auto& x : assignStmtsWithVar) {
     VAR_NAME varName = t_pkb->getVarNameFromIdx(x.second);
     assignStmtsWithVarName[x.first] = varName;
-  }
+  }*/
 
-  m_result = Formatter::formatMapIntStrToMapStrVectorStr(assignStmtsWithVarName);
+  m_result = Formatter::formatMapIntIntToMapIntVectorInt(assignStmtsWithVar);
   return m_result;
 }
 
-SET_OF_RESULTS AssignPatternEvaluator::getAllStmtsAndVarWithAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::unordered_map<int, std::string> assignStmtsWithVar = t_pkb->getAllAssignStmtWithVarName();
+SET_OF_RESULTS_INDICES AssignPatternEvaluator::getAllStmtsAndVarWithAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  MAP_OF_STMT_NUM_TO_VAR_INDEX assignStmtsWithVar = t_pkb->getAllAssignStmtWithVarIndex();
   if (assignStmtsWithVar.empty()) {
     return m_result;
   }
 
-  m_result = Formatter::formatMapIntStrToMapStrVectorStr(assignStmtsWithVar);
+  m_result = Formatter::formatMapIntIntToMapIntVectorInt(assignStmtsWithVar);
   return m_result;
 }
--- source/WhilePatternEvaluator.cpp
+++ source/WhilePatternEvaluator.cpp
@@ -2,116 +2,116 @@
 
 #include "WhilePatternEvaluator.h"
 
-SET_OF_RESULTS WhilePatternEvaluator::getAllStmtsWithVarAndExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::vector<int> whileStmts = t_pkb->getWhileStmtByVar(t_g1.getName());
+SET_OF_RESULTS_INDICES WhilePatternEvaluator::getAllStmtsWithVarAndExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  LIST_OF_STMT_NUMS whileStmts = t_pkb->getWhileStmtByVar(t_g1.getName());
   if (whileStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(whileStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(whileStmts);
+  m_result[0] = whileStmts;
   return m_result;
 }
 
-SET_OF_RESULTS WhilePatternEvaluator::getAllStmtsWithVarAndSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::vector<int> whileStmts = t_pkb->getWhileStmtByVar(t_g1.getName());
+SET_OF_RESULTS_INDICES WhilePatternEvaluator::getAllStmtsWithVarAndSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  LIST_OF_STMT_NUMS whileStmts = t_pkb->getWhileStmtByVar(t_g1.getName());
   if (whileStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(whileStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(whileStmts);
+  m_result[0] = whileStmts;
   return m_result;
 }
 
-SET_OF_RESULTS WhilePatternEvaluator::getAllStmtsWithVarAndAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::vector<int> whileStmts = t_pkb->getWhileStmtByVar(t_g1.getName());
+SET_OF_RESULTS_INDICES WhilePatternEvaluator::getAllStmtsWithVarAndAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  LIST_OF_STMT_NUMS whileStmts = t_pkb->getWhileStmtByVar(t_g1.getName());
   if (whileStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(whileStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(whileStmts);
+  m_result[0] = whileStmts;
   return m_result;
 }
 
-SET_OF_RESULTS WhilePatternEvaluator::getAllStmtsWithExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::vector<int> whileStmts = t_pkb->getAllWhileStmts();
+SET_OF_RESULTS_INDICES WhilePatternEvaluator::getAllStmtsWithExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  LIST_OF_STMT_NUMS whileStmts = t_pkb->getAllWhileStmts();
   if (whileStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(whileStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(whileStmts);
+  m_result[0] = whileStmts;
   return m_result;
 }
 
-SET_OF_RESULTS WhilePatternEvaluator::getAllStmtsWithSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::vector<int> whileStmts = t_pkb->getAllWhileStmts();
+SET_OF_RESULTS_INDICES WhilePatternEvaluator::getAllStmtsWithSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  LIST_OF_STMT_NUMS whileStmts = t_pkb->getAllWhileStmts();
   if (whileStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(whileStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(whileStmts);
+  m_result[0] = whileStmts;
   return m_result;
 }
 
-SET_OF_RESULTS WhilePatternEvaluator::getAllStmtsWithAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::vector<int> allWhileStmts = t_pkb->getAllWhileStmts();
+SET_OF_RESULTS_INDICES WhilePatternEvaluator::getAllStmtsWithAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  LIST_OF_STMT_NUMS allWhileStmts = t_pkb->getAllWhileStmts();
   if (allWhileStmts.empty()) {
     return m_result;
   }
 
-  std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(allWhileStmts);
-  m_result[t_stmt.getName()] = stmtVector;
+  //std::vector<std::string> stmtVector = Formatter::formatVectorIntToVectorStr(allWhileStmts);
+  m_result[0] = allWhileStmts;
   return m_result;
 }
 
-SET_OF_RESULTS WhilePatternEvaluator::getAllStmtsAndVarWithExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::unordered_map<STMT_NUM, VAR_INDEX> whileStmtsWithVar = t_pkb->getAllWhileStmtsWithVar();
+SET_OF_RESULTS_INDICES WhilePatternEvaluator::getAllStmtsAndVarWithExactPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  MAP_OF_STMT_NUM_TO_VAR_INDEX whileStmtsWithVar = t_pkb->getAllWhileStmtsWithVar();
   if (whileStmtsWithVar.empty()) {
     return m_result;
   }
 
-  std::unordered_map<STMT_NUM, VAR_NAME> whileStmtsWithVarName;
+  /*std::unordered_map<STMT_NUM, VAR_NAME> whileStmtsWithVarName;
   for (auto& x : whileStmtsWithVar) {
     VAR_NAME varName = t_pkb->getVarNameFromIdx(x.second);
     whileStmtsWithVarName[x.first] = varName;
-  }
+  }*/
 
-  m_result = Formatter::formatMapIntStrToMapStrVectorStr(whileStmtsWithVarName);
+  m_result = Formatter::formatMapIntIntToMapIntVectorInt(whileStmtsWithVar);
   return m_result;
 }
 
-SET_OF_RESULTS WhilePatternEvaluator::getAllStmtsAndVarWithSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::unordered_map<STMT_NUM, VAR_INDEX> whileStmtsWithVar = t_pkb->getAllWhileStmtsWithVar();
+SET_OF_RESULTS_INDICES WhilePatternEvaluator::getAllStmtsAndVarWithSubPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  MAP_OF_STMT_NUM_TO_VAR_INDEX whileStmtsWithVar = t_pkb->getAllWhileStmtsWithVar();
   if (whileStmtsWithVar.empty()) {
     return m_result;
   }
 
-  std::unordered_map<STMT_NUM, VAR_NAME> whileStmtsWithVarName;
+  /*std::unordered_map<STMT_NUM, VAR_NAME> whileStmtsWithVarName;
   for (auto& x : whileStmtsWithVar) {
     VAR_NAME varName = t_pkb->getVarNameFromIdx(x.second);
     whileStmtsWithVarName[x.first] = varName;
-  }
+  }*/
 
-  m_result = Formatter::formatMapIntStrToMapStrVectorStr(whileStmtsWithVarName);
+  m_result = Formatter::formatMapIntIntToMapIntVectorInt(whileStmtsWithVar);
   return m_result;
 }
 
-SET_OF_RESULTS WhilePatternEvaluator::getAllStmtsAndVarWithAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
-  std::unordered_map<STMT_NUM, VAR_INDEX> whileStmtsWithVar = t_pkb->getAllWhileStmtsWithVar();
+SET_OF_RESULTS_INDICES WhilePatternEvaluator::getAllStmtsAndVarWithAnyPattern(PkbReadOnly *t_pkb, Grammar t_stmt, Grammar t_g1, Grammar t_g2) {
+  MAP_OF_STMT_NUM_TO_VAR_INDEX whileStmtsWithVar = t_pkb->getAllWhileStmtsWithVar();
   if (whileStmtsWithVar.empty()) {
     return m_result;
   }
 
-  std::unordered_map<STMT_NUM, VAR_NAME> whileStmtsWithVarName;
+  /*std::unordered_map<STMT_NUM, VAR_NAME> whileStmtsWithVarName;
   for (auto& x : whileStmtsWithVar) {
     VAR_NAME varName = t_pkb->getVarNameFromIdx(x.second);
     whileStmtsWithVarName[x.first] = varName;
-  }
+  }*/
 
-  m_result = Formatter::formatMapIntStrToMapStrVectorStr(whileStmtsWithVarName);
+  m_result = Formatter::formatMapIntIntToMapIntVectorInt(whileStmtsWithVar);
   return m_result;
 }
