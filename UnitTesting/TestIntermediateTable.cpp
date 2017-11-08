093f98f7aadcd90ae7ae29f48f2184fb6caad01c
Verbena <verbena_94@hotmail.com>
2017-11-09 03:28:13 +0800

    Refactor string to integer in intermediate table class, test-driver and unit testing.

--- UnitTesting/TestIntermediateTable.cpp
+++ UnitTesting/TestIntermediateTable.cpp
@@ -21,8 +21,7 @@ namespace UnitTesting {
       2  'b'  3   4
       3  'c'  4   5
       */
-      INTERMEDIATE_TABLE result = { {"1", "a", "2", "3"}, 
-      {"2", "b", "3", "4"}, {"3","c","4", "5"}};
+      INTERMEDIATE_TABLE result = { { 1, 1, 2, 3 }, { 2, 2, 3, 4 }, { 3, 3, 4, 5 } };
       m_driver->insertTable(result);
       m_driver->insertSynonymRow({"a", "v", "s1", "s2"});
     }
@@ -85,21 +84,21 @@ namespace UnitTesting {
       m_driver->clearTable();
 
       //Test the inserting of one synonym when the table is empty
-      LIST_OF_RESULTS result = { "1", "3", "7" };
+      LIST_OF_RESULTS_INDICES result = { 1, 3, 7 };
       INTERMEDIATE_TABLE actual = m_driver->TestInsertOneSynonym("s1", result);
-      INTERMEDIATE_TABLE expected = { { "1" },{ "3" },{ "7" } };
+      INTERMEDIATE_TABLE expected = { { 1 },{ 3 },{ 7 } };
       Assert::IsTrue(actual == expected);
 
       //Test the inserting of one synonym when the synonym is not inside the non-empty table
-      result = { "2", "4" };
+      result = { 2, 4 };
       actual = m_driver->TestInsertOneSynonym("s2", result);
-      expected = { { "1", "2" },{ "3", "2" },{ "7", "2" },{ "1", "4" },{ "3", "4" },{ "7", "4" } };
+      expected = { { 1, 2 },{ 3, 2 },{ 7, 2 },{ 1, 4 },{ 3, 4 },{ 7, 4 } };
       Assert::IsTrue(actual == expected);
 
       //Test the inserting of one synonym when the synonym is inside the non-empty table
-      result = { "1", "3", "4" };
+      result = { 1, 3, 4 };
       actual = m_driver->TestInsertOneSynonym("s2", result);
-      expected = { { "1", "4" },{ "3", "4" },{ "7", "4" } };
+      expected = { { 1, 4 },{ 3, 4 },{ 7, 4 } };
       Assert::IsTrue(actual == expected);
     }
 
@@ -108,36 +107,36 @@ namespace UnitTesting {
       m_driver->clearTable();
 
       //Test the inserting of two synonyms when the table is empty
-      SET_OF_RESULTS result = { { "1",{ "4" } },{ "3",{ "4" } },{ "7",{ "4" } } };
+      SET_OF_RESULTS_INDICES result = { { 1, { 4 } },{ 3, { 4 } },{ 7, { 4 } } };
       INTERMEDIATE_TABLE actual = m_driver->TestInsertTwoSynonym("s1", "s2", result);
       INTERMEDIATE_TABLE expected = { { "1", "4" },{ "3", "4" },{ "7", "4" } };
       Assert::IsTrue(actual == expected);
 
       //Test the inserting of two synonyms when both synonyms are not inside the non-empty table
-      result = { { "2", { "3", "4", "5" } },{ "3", { "4", "5" } },{ "4", { "5" } } };
+      result = { { 2, { 3, 4, 5 } },{ 3, { 4, 5 } },{ 4, { 5 } } };
       actual = m_driver->TestInsertTwoSynonym("s3", "s4", result);
       expected = { 
-      { "1", "4", "2", "3" },{ "3", "4", "2", "3" },{ "7", "4", "2", "3" },
-      { "1", "4", "2", "4" },{ "3", "4", "2", "4" },{ "7", "4", "2", "4" },
-      { "1", "4", "2", "5" },{ "3", "4", "2", "5" },{ "7", "4", "2", "5" },
-      { "1", "4", "3", "4" },{ "3", "4", "3", "4" },{ "7", "4", "3", "4" },
-      { "1", "4", "3", "5" },{ "3", "4", "3", "5" },{ "7", "4", "3", "5" },
-      { "1", "4", "4", "5" },{ "3", "4", "4", "5" },{ "7", "4", "4", "5" } };
+      { 1, 4, 2, 3 },{ 3, 4, 2, 3 },{ 7, 4, 2, 3 },
+      { 1, 4, 2, 4 },{ 3, 4, 2, 4 },{ 7, 4, 2, 4 },
+      { 1, 4, 2, 5 },{ 3, 4, 2, 5 },{ 7, 4, 2, 5 },
+      { 1, 4, 3, 4 },{ 3, 4, 3, 4 },{ 7, 4, 3, 4 },
+      { 1, 4, 3, 5 },{ 3, 4, 3, 5 },{ 7, 4, 3, 5 },
+      { 1, 4, 4, 5 },{ 3, 4, 4, 5 },{ 7, 4, 4, 5 } };
       Assert::IsTrue(actual == expected);
 
       //Test the inserting of two synonyms when one of the synonym is inside the non-empty table
-      result = { { "1", { "2", "3", "4" } },{ "2", { "3", "4", "5" } },{ "4", { "5", "6" } },{ "6", { "7" } } };
+      result = { { 1, { 2, 3, 4 } },{ 2, { 3, 4, 5 } },{ 4, { 5, 6 } },{ 6, { 7 } } };
       actual = m_driver->TestInsertTwoSynonym("s4", "s5", result);
       expected = {
-        { "1", "4", "2", "4", "5" },{ "1", "4", "2", "4", "6" },{ "3", "4", "2", "4", "5" },{ "3", "4", "2", "4", "6" },
-        { "7", "4", "2", "4", "5" },{ "7", "4", "2", "4", "6" },{ "1", "4", "3", "4", "5" },{ "1", "4", "3", "4", "6" },
-        { "3", "4", "3", "4", "5" },{ "3", "4", "3", "4", "6" },{ "7", "4", "3", "4", "5" },{ "7", "4", "3", "4", "6" } };
+        { 1, 4, 2, 4, 5 },{ 1, 4, 2, 4, 6 },{ 3, 4, 2, 4, 5 },{ 3, 4, 2, 4, 6 },
+        { 7, 4, 2, 4, 5 },{ 7, 4, 2, 4, 6 },{ 1, 4, 3, 4, 5 },{ 1, 4, 3, 4, 6 },
+        { 3, 4, 3, 4, 5 },{ 3, 4, 3, 4, 6 },{ 7, 4, 3, 4, 5 },{ 7, 4, 3, 4, 6 } };
       Assert::IsTrue(actual == expected);
 
       //Test the inserting of two synonyms when both synonyms are inside the non-empty table
-      result = { { "1", { "4", "6", "7" } },{ "2", { "3", "4", "5" } }, { "3",{ "7", "8" } } };
+      result = { { 1, { 4, 6, 7 } },{ 2, { 3, 4, 5 } }, { 3,{ 7, 8 } } };
       actual = m_driver->TestInsertTwoSynonym("s1", "s5", result);
-      expected = { { "1", "4", "2", "4", "6" },{ "1", "4", "3", "4", "6" } };
+      expected = { { 1, 4, 2, 4, 6 },{ 1, 4, 3, 4, 6 } };
       Assert::IsTrue(actual == expected);
     }
   };
