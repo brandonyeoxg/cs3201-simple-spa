6d83c8da26babaec622e76d91abad97a0175b2a1
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2017-11-10 00:38:35 +0800

    Created function to generate if and while stmts; Shortened the test file so that I can put stuff in it later

--- AutoTester/Tests/System/Combo/Prog Generator.py
+++ AutoTester/Tests/System/Combo/Prog Generator.py
@@ -1,3 +1,4 @@
+from random import randint
 def gen(num):
     for i in range(num):
         print("  stress"+str(i)+" = test"+str(i+1)+";")
@@ -17,4 +18,49 @@ def gen4(num):
     for i in range(num%7, round(num*3-1)):
         print("  master"+str(abs(i))+" = Hacka"+str(i+3*num)+";")
     return None
+
+def genWhile(nest,string,depth):
+    if not(depth==nest):
+        string += (nest * "  ") + "while i" + str(nest)+"{\n"
+        return genWhile(nest+1,string,depth)
+    else:
+        a = randint(1,5)
+        if (a==1):
+            return string+"\n"+((nest-1) * "  ")+(nest * "}")
+        elif (a==2):
+            return string+((nest) * "  ")+(nest * "}")
+        elif (a==3):
+            i=nest
+            while not(i==0):
+                string += (i*"  ")+"}\n"
+                i=i-1
+            return string
+        else:
+            i=nest
+            while not(i==0):
+                i=i-1
+                string += "\n"+(i*"  ")+"}"
+            return string
+
+def genIf(nest,string,depth):
+    if not(depth==nest):
+        string += (nest * "  ") + "if i" + str(nest)+"{\n"
+        return genIf(nest+1,string,depth)
+    else:
+        a = randint(1,2)
+        if (a==1):
+            i=nest
+            while not(i==0):
+                i=i-1
+                string += (i*"  ")+"}\n"
+                string += (i*"  ") + "else{\n" + (i*"  ") +"}\n"
+            return string
+        else:
+            i=nest
+            while not(i==0):
+                i=i-1
+                string += "\n"+(i*"  ")+"}else{\n"+((i+1)*"  ")+"\n"+(i*"  ")+"}"
+            return string
+
+print(genIf(0,"",10))
 
