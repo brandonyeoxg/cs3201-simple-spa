49614b46a43591dd01e65d5faef230b1ce810845
Verbena <verbena_94@hotmail.com>
2017-11-10 17:46:00 +0800

    Move all the automate batch files to a folder.

---
+++ AutoTester/Automation/autotester_automate_extra.bat
@@ -0,0 +1,6 @@
+start "" "%~dp0../../Debug/AutoTester.exe" %~dp0../Tests/Extra/source_std1.txt %~dp0../Tests/Extra/std1_basic.txt %~dp0../Output/DebugOutput/extra-basic-out1.xml
+start "" "%~dp0../../Debug/AutoTester.exe" %~dp0../Tests/Extra/source_std1.txt %~dp0../Tests/Extra/std1_adv.txt %~dp0../Output/DebugOutput/extra-adv-out1.xml
+start "" "%~dp0../../Debug/AutoTester.exe" %~dp0../Tests/Extra/source_std2.txt %~dp0../Tests/Extra/std2_basic.txt %~dp0../Output/DebugOutput/extra-basic-out2.xml
+start "" "%~dp0../../Debug/AutoTester.exe" %~dp0../Tests/Extra/source_std2.txt %~dp0../Tests/Extra/std2_adv.txt %~dp0../Output/DebugOutput/extra-adv-out2.xml
+start "" "%~dp0../../Debug/AutoTester.exe" %~dp0../Tests/Extra/source_std3.txt %~dp0../Tests/Extra/std3_basic.txt %~dp0../Output/DebugOutput/extra-basic-out3.xml
+start "" "%~dp0../../Debug/AutoTester.exe" %~dp0../Tests/Extra/source_std3.txt %~dp0../Tests/Extra/std3_adv.txt %~dp0../Output/DebugOutput/extra-adv-out3.xml
--- AutoTester/autotester_automate_extra_release.bat
+++
@@ -1,6 +0,0 @@
-start "" "%~dp0../Release/AutoTester.exe" %~dp0/Tests/Extra/source_std1.txt %~dp0/Tests/Extra/std1_basic.txt %~dp0/Output/ReleaseOutput/extra-basic-out1.xml
-start "" "%~dp0../Release/AutoTester.exe" %~dp0/Tests/Extra/source_std1.txt %~dp0/Tests/Extra/std1_adv.txt %~dp0/Output/ReleaseOutput/extra-adv-out1.xml
-start "" "%~dp0../Release/AutoTester.exe" %~dp0/Tests/Extra/source_std2.txt %~dp0/Tests/Extra/std2_basic.txt %~dp0/Output/ReleaseOutput/extra-basic-out2.xml
-start "" "%~dp0../Release/AutoTester.exe" %~dp0/Tests/Extra/source_std2.txt %~dp0/Tests/Extra/std2_adv.txt %~dp0/Output/ReleaseOutput/extra-adv-out2.xml
-start "" "%~dp0../Release/AutoTester.exe" %~dp0/Tests/Extra/source_std3.txt %~dp0/Tests/Extra/std3_basic.txt %~dp0/Output/ReleaseOutput/extra-basic-out3.xml
-start "" "%~dp0../Release/AutoTester.exe" %~dp0/Tests/Extra/source_std3.txt %~dp0/Tests/Extra/std3_adv.txt %~dp0/Output/ReleaseOutput/extra-adv-out3.xml
